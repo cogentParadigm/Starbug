046f57037695b5380394056b00498d2287ff222b
Ali Gangji <cogent@gmail.com>
2009-02-04 23:32:02 +0000

    ..cleaning up rename errors

---
+++ core/app/nouns/Ajax/uris/add.php
@@ -0,0 +1,7 @@
+<div id="add_uri" class="box">
+		<?php $formid = "add_uri_form"; $action = "create"; include("core/app/nouns/uris/uris_form.php"); ?>
+		<ul class="buttons">
+			<li><a class="button" href="#" onclick="save_add();return false;">Save</a></li>
+			<li><a class="button" href="#" onclick="cancel_add();return false;">Cancel</a></li>
+		</ul>
+</div>
