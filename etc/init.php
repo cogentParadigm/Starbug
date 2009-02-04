0d98177924159db68ad6e94cdf1fd1401f04aec3
Ali Gangji <cogent@gmail.com>
2009-02-04 02:24:34 +0000

    license changed to gpl

--- etc/init.php
+++ etc/init.php
@@ -1,20 +1,22 @@
 <?php
 /**
-* Starbug - PHP web service development kit
+* This file is part of StarbugPHP
+*
+* StarbugPHP - web service development kit
 * Copyright (C) 2008-2009 Ali Gangji
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
+* StarbugPHP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 *
-* This program is distributed in the hope that it will be useful,
+* StarbugPHP is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
+* GNU General Public License for more details.
 *
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU General Public License
+* along with StarbugPHP.  If not, see <http://www.gnu.org/licenses/>.
 */
 function load_file($location) {return require_once($location.".php");}
 include("core/db/adodb_lite/adodb.inc.php");
--- script/_generate/model.php
+++ script/_generate/model.php
@@ -2,21 +2,23 @@
 /**
 * generates a model that extends the Table class
 *
-* Starbug - PHP web service development kit
+* This file is part of StarbugPHP
+*
+* StarbugPHP - web service development kit
 * Copyright (C) 2008-2009 Ali Gangji
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
+* StarbugPHP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 *
-* This program is distributed in the hope that it will be useful,
+* StarbugPHP is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
+* GNU General Public License for more details.
 *
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU General Public License
+* along with StarbugPHP.  If not, see <http://www.gnu.org/licenses/>.
 */
 $file = fopen(dirname(__FILE__)."/../../app/models/".ucwords($argv[2]).".php", "w");
 fwrite($file, "<?php\nclass ".ucwords($argv[2])." extends Table {\n\n");
