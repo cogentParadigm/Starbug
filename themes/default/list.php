31d434427b0dde6137ab8c1819cb5d54aa30f3fb
Ali Gangji <cogent@gmail.com>
2009-04-02 00:04:26 +0000

    one more file..

---
+++ themes/default/list.php
@@ -0,0 +1,6 @@
+<ul class="thumbnails">
+	<li><a href="<?php echo uri("generate/app"); ?>"><img src="<?php echo $themedir."public/images/app.png"; ?>" /><span class="label">Bare Application</span></a></li>
+	<li><a href="<?php echo uri("generate/model"); ?>"><img src="<?php echo $themedir."public/images/model.png"; ?>" /><span class="label">Database Model</span></a></li>
+	<li><a href="<?php echo uri("generate/crud"); ?>"><img src="<?php echo $themedir."public/images/crud.png"; ?>" /><span class="label">CRUD</span></a></li>
+</ul>
+<div class="clear"></div>
