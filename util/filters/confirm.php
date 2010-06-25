b1f862eedcfb9916655dd15a7bd7728e975c42c8
Ali Gangji <cogent@gmail.com>
2010-06-25 00:53:57 -0700

    confirm and optional_update filters bugs

--- util/filters/confirm.php
+++ util/filters/confirm.php
@@ -2,5 +2,6 @@
 foreach($args as $field => $confirm) {
 	if ((isset($fields[$confirm])) && ($fields[$field] != $fields[$confirm])) $errors[$field]["confirm"] = "Your $field"."s do not match";
 	unset($fields[$confirm]);
+	unset($errors[$confirm]['required']);
 }
 ?>
--- util/filters/optional_update.php
+++ util/filters/optional_update.php
@@ -1,5 +1,8 @@
 <?php
 foreach($args as $field => $optional) {
-	if ((!empty($fields['id'])) && (empty($fields[$field]))) unset($field[$field]);
+	if ((!empty($fields['id'])) && (empty($fields[$field]))) {
+		unset($fields[$field]);
+		unset($errors[$field]['required']);
+	}
 }
 ?>
