e797d4493bf0de370df46391401311783c816193
Ali Gangji <cogent@gmail.com>
2010-09-29 15:31:26 -0700

    updated generator xml - only one doc per model now

--- app/themes/default/crud/update.php
+++ app/themes/default/crud/update.php
@@ -1,8 +1,9 @@
 <?php
 	$dirs = array("app/views/$model_name");
-	$from_model = array("crud/create.xsl" => "app/views/$model_name/create.php",
-											"crud/update.xsl" => "app/views/$model_name/update.php",
-											"crud/list.xsl" => "app/views/$model_name/default.php"
+	$generate = array(
+		"crud/create.xsl" => "app/views/$model_name/create.php",
+		"crud/update.xsl" => "app/views/$model_name/update.php",
+		"crud/list.xsl" => "app/views/$model_name/default.php",
+		"crud/form.xsl" => "app/views/$model_name/form.php"
 	);
-	$from_form = array("crud/form.xsl" => "app/views/$model_name/form.php");
 ?>
