2605575a0d89e909883c003b2969477384868233
Ali Gangji <cogent@gmail.com>
2010-11-05 19:24:18 -0700

    removed datepicker and added installer for datepicker

--- util/datepicker.php
+++ util/datepicker.php
@@ -1,3 +1,2 @@
 <?php $sb->provide("util/datepicker"); ?>
-<style type="text/css">@import url(<?php echo uri("app/public/stylesheets/datepicker.css"); ?>);</style>
-<script type="text/javascript" src="<?php echo uri("app/public/js/datepicker.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo uri("app/public/js/datepicker/datepicker.js"); ?>"></script>
