533b0a74d6caf00ff7cfcb781753487464f0942d
Ali Gangji <cogent@gmail.com>
2010-11-06 21:07:32 -0700

    removed user auth from plugin, letting plugins lie low for 0.9

--- app/models/Users.php
+++ app/models/Users.php
@@ -9,5 +9,24 @@ class Users extends UsersModel {
 	function delete() {
 		return $this->remove("id=".$_POST['users']['id']);
 	}
+
+	function login() {
+		$errors = array();
+		$login = $_POST['users'];
+		$user = $this->query("select:id, memberships  where:username='".$login['username']."' && password='".md5($login['password'])."'  limit:1");
+		if (!empty($user)) {
+			$_SESSION[P("id")] = $user['id'];
+			$_SESSION[P("memberships")] = $user['memberships'];
+		} else {
+			$errors['username']['loginMatch'] = "That username and password combination was not found.";
+		}
+		return $errors;
+	}
+
+	function logout() {
+		$_SESSION[P("id")] = 0;
+		$_SESSION[P("memberships")] = 0;
+		return array();
+	}
 }
 ?>
