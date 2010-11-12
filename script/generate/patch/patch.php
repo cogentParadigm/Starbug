31a24b3d20e40668662cdb6bf1af90afbab9f76b
Ali Gangji <cogent@gmail.com>
2010-11-11 23:15:24 -0800

    added handling for uri elements in patch xsl

--- script/generate/patch/patch.php
+++ script/generate/patch/patch.php
@@ -1,7 +1,9 @@
 <?php
-	XMLBuilder::write_model($model, $schemer->tables[$model]);
-	$dirs = array("patch", "patch/app", "patch/app/migrations");
-	$generate = array(
-		"patch/migration.xsl" => "patch/app/migrations/".ucwords($model)."Migration.php"
-	);
+	if (empty($models)) foreach ($schemer->tables as $name => $fields) passthru("sb generate patch $name");
+	else {
+		XMLBuilder::write_model($model, $schemer->tables[$model]);
+		$dirs = array("patch", "patch/app", "patch/app/migrations");
+		$generate = array(
+			"patch/migration.xsl" => "patch/app/migrations/".ucwords($model)."Migration.php"
+		);
 ?>
