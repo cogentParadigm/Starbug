414f48ff014be661c81bb9b1bac7137106d28e0a
Ali Gangji <cogent@gmail.com>
2010-11-14 22:03:12 -0800

    added migration list to patch

--- script/generate/patch/patch.php
+++ script/generate/patch/patch.php
@@ -1,10 +1,14 @@
 <?php
-	if (empty($model)) foreach ($schemer->tables as $name => $fields) passthru("sb generate patch $name");
-	else {
+	$dirs = array("patch", "patch/app", "patch/app/migrations", "patch/app/hooks");
+	if (empty($model)) {
+		foreach ($schemer->tables as $name => $fields) passthru("sb generate patch $name");
+	} else {
 		XMLBuilder::write_model($model, $schemer->tables[$model]);
-		$dirs = array("patch", "patch/app", "patch/app/migrations");
 		$generate = array(
 			"patch/migration.xsl" => "patch/app/migrations/".ucwords($model)."Migration.php"
 		);
+		if (!file_exists(BASE_DIR."/patch/app/hooks/global.migrations")) $hook = array(10 => array());
+		else $hook = unserialize(file_get_contents(BASE_DIR."/patch/app/hooks/global.migrations"));
+		$hook[10][] = array("handle" => "return_it", "args" => ucwords($model)."Migration.php");
 	}
 ?>
