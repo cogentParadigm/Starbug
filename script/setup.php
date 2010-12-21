e6d985b9f2c08ec8398164404f38b7bf14235d4f
Ali Gangji <cogent@gmail.com>
2010-12-21 05:49:22 -0800

    added app templates for setup in /etc/setup that get created if they don't exist

--- script/setup.php
+++ script/setup.php
@@ -6,30 +6,33 @@
  * @author Ali Gangji <ali@neonrain.com>
  * @ingroup script
  */
+	if (!defined('BASE_DIR')) define('BASE_DIR', str_replace("/script", "", dirname(__FILE__)));
+	if (!defined('STDOUT')) define("STDOUT", fopen("php://stdout", "wb"));
+	if (!defined('STDIN')) define("STDIN", fopen("php://stdin", "r"));
+	$host = (file_exists(BASE_DIR."/etc/Host.php"));
 
 	//CREATE FOLDERS & SET FILE PERMISSIONS
 	$dirs = array("var", "var/xml", "var/models", "var/tmp", "var/public", "var/public/stylesheets", "app/public/js", "app/public/uploads", "app/public/thumbnails");
 	foreach ($dirs as $dir) if (!file_exists(BASE_DIR."/".$dir)) exec("mkdir ".BASE_DIR."/".$dir);
 	exec("chmod -R a+w ".BASE_DIR."/var ".BASE_DIR."/app/hooks ".BASE_DIR."/app/public/uploads ".BASE_DIR."/app/public/thumbnails");
-	if (!file_exists(BASE_DIR."/var/migration")) {
-		$file = fopen(BASE_DIR."/var/migration", "wb");
-		fwrite($file, "0");
-		fclose($file);
+	exec("cp -an ".BASE_DIR."/etc/setup/* ".BASE_DIR."/");
+
+	if ($host) {
+		//INIT TABLES
+		$schemer->migrate();
+	
+		$root = query("users", "where:username='root'  limit:1");
+		if ($root['modified'] == $root['created']) { // PASSWORD HAS NEVER BEEN UPDATED
+			//COLLECT USER INPUT
+			fwrite(STDOUT, "\nPlease choose a root password:");
+			$admin_pass = str_replace("\n", "", fgets(STDIN));
+			fwrite(STDOUT, "\n\nYou may log in with these credentials -");
+			fwrite(STDOUT, "\nusername: root");
+			fwrite(STDOUT, "\npassword: $admin_pass\n\n");
+			//UPDATE PASSWORD
+			$errors = store("users", "password:$admin_pass", "username:root");
+		}
+	} else {
+		fwrite(STDOUT, "\netc/Host.php has been generated. Enter your host details and run setup again.\n\n");
 	}
-
-	//INIT TABLES
-	$schemer->migrate();
-
-	$root = query("users", "where:username='root'  limit:1");
-	if ($root['modified'] == $root['created']) { // PASSWORD HAS NEVER BEEN UPDATED
-		//COLLECT USER INPUT
-		fwrite(STDOUT, "\nPlease choose a root password:");
-		$admin_pass = str_replace("\n", "", fgets(STDIN));
-		fwrite(STDOUT, "\n\nYou may log in with these credentials -");
-		fwrite(STDOUT, "\nusername: root");
-		fwrite(STDOUT, "\npassword: $admin_pass\n\n");
-		//UPDATE PASSWORD
-		$errors = store("users", "password:$admin_pass", "username:root");
-	}
-
 ?>
