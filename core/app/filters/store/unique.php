d3ef364deead6e313364c548ed7a827ac344167d
Ali Gangji <cogent@gmail.com>
2010-12-21 03:57:10 -0800

    fixed bug in unique filter

--- core/app/filters/store/unique.php
+++ core/app/filters/store/unique.php
@@ -1,6 +1,6 @@
 <?php
 foreach($args as $field => $unique) {
-$urow = $this->query($name, "select:id, $field  where:$field='".$fields[$field]."'  limit:1");
-if (((!empty($fields['id'])) || ($this->record_count != 0)) && ((empty($fields['id'])) || ($this->record_count > 1) || ($fields['id'] != $urow['id']))) $errors[$field]["exists"] = "That $field already exists.";
+$urow = $this->query($name, "select:id, $field  where:$field='".$fields[$field]."'".((empty($fields['id'])) ? "" : " && id!='$fields[id]'")."  limit:1");
+if ($this->record_count != 0) $errors[$field]["exists"] = "That $field already exists.";
 }
 ?>
