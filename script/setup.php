947e21dc6a2b65f5106a75331077a9b4cddafe6c
Ali Gangji <cogent@gmail.com>
2011-01-30 20:19:14 -0800

    setup script bug

--- script/setup.php
+++ script/setup.php
@@ -12,9 +12,9 @@
 	$host = (file_exists(BASE_DIR."/etc/Host.php"));
 
 	//CREATE FOLDERS & SET FILE PERMISSIONS
-	$dirs = array("var", "var/xml", "var/models", "var/tmp", "var/public", "var/public/stylesheets", "app/public/js", "app/public/uploads", "app/public/thumbnails");
+	$dirs = array("var", "var/xml", "var/models", "var/tmp", "var/public", "var/public/stylesheets", "app/hooks", "app/public/js", "app/public/uploads", "app/public/thumbnails");
 	foreach ($dirs as $dir) if (!file_exists(BASE_DIR."/".$dir)) exec("mkdir ".BASE_DIR."/".$dir);
-	exec("chmod -R a+w ".BASE_DIR."/var ".BASE_DIR."/app/hooks ".BASE_DIR."/app/public/uploads ".BASE_DIR."/app/public/thumbnails");
+	exec("chmod -R a+w ".BASE_DIR."/var ".BASE_DIR."/app/public/uploads ".BASE_DIR."/app/public/thumbnails");
 	exec("cp -an ".BASE_DIR."/etc/setup/* ".BASE_DIR."/");
 
 	if ($host) {
