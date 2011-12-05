3f1d7f4896491a97a778004f75483a906b02f7af
Ali Gangji <cogent@gmail.com>
2011-12-05 08:43:22 -0800

    updated ordered filter

--- core/app/filters/store/ordered.php
+++ core/app/filters/store/ordered.php
@@ -1,22 +1,22 @@
 <?php
 foreach ($args as $field => $ordered) {
-	$ordered_items = $name."_".$field."_".$order;
-	global $$ordered_items;
-	if ($storing) {
-		if (!empty($$ordered_items)) $$ordered_items = "";
-	} else {
-		$on_store = true;
-		if (empty($fields['id'])) {
-			$h = $this->query($name, "select:MAX(`$field`) as highest  limit:1");
-			$fields[$field] = $h['highest']+1;
-		} else if (!empty($fields[$field])) {
-			$x = $fields[$field];
-			$old = $this->query($name, "select:`$field`  where:`id`='$fields[id]'  limit:1");
-			$increment = ($old[$field] < $x) ? -1 : 1;
-			if (empty($$ordered_items)) $$ordered_items = $fields['id'];
-			else $$ordered_items .= ",".$fields['id'];
-			$row = $this->query($name, "where:`id` NOT IN (".$$ordered_items.") && `$field`='$x'  limit:1");
-			if (!empty($row)) $this->queue($name, "id:$row[id]  $field:".($x+$increment));
+	if (!empty($ordered)) $where = $ordered."='".$fields[$ordered]."' && ";
+	else $where = "";
+	if (empty($fields['id'])) {
+		$h = $this->query($name, "select:MAX(`$field`) as highest  where:$where"."1  limit:1");
+		$fields[$field] = $h['highest']+1;
+	} else if (!empty($fields[$field])) {
+		$x = $fields[$field];
+		unset($fields[$field]);
+		$row = $this->query($name, "select:id,$field  where:id='$fields[id]'  limit:1");
+		$ids = $row['id'];
+		$increment = ($row[$field] < $x) ? -1 : 1;
+		//$increment = 1;
+		while (!empty($row)) {
+			raw_query("UPDATE ".P($name)." SET $field='$x' where id='$row[id]'");
+			$row = $this->query($name, "where:$where"."id NOT IN ($ids) && `$field`='$x'  limit:1");
+			$ids .= ", ".$row['id'];
+			$x += $increment;
 		}
 	}
 }
