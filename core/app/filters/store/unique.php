f7dc78e96f2d7773774bcc4f17a133caec7dcdff
Ali Gangji <cogent@gmail.com>
2011-12-12 10:22:57 -0800

    unique filter updated to use prepared statement

--- core/app/filters/store/unique.php
+++ core/app/filters/store/unique.php
@@ -1,6 +1,6 @@
 <?php
 foreach($args as $field => $unique) {
-$urow = $this->query($name, "select:id, $field  where:$field='".$fields[$field]."'".((empty($fields['id'])) ? "" : " && id!='$fields[id]'")."  limit:1");
+$urow = $this->query($name, "select:id, $field  where:$field=?".((empty($fields['id'])) ? "" : " && id!='$fields[id]'")."  limit:1", array($fields[$field]));
 if ($this->record_count != 0) $errors[$field]["exists"] = "That $field already exists.";
 }
 ?>
