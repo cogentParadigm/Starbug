78f8a41393436b04ccd928a760275f4fdeac7e13
Ali Gangji <cogent@gmail.com>
2011-12-16 10:18:22 -0800

    removed debug output

--- core/app/templates/form/block/text.php
+++ core/app/templates/form/block/text.php
@@ -1,5 +1,4 @@
 <?php
-echo $id;
 $_POST["block-$region-$position"] = $sb->query("uris,blocks", "select:blocks.*  where:uris.id='$id' && region='$region' && position='$position'  limit:1");
 $form = new form("model:block-$region-$position");
 echo $form->textarea("content  nolabel:true  id:block-$region-$position  style:width:100%");
