7fcfceb493cd010e2b5aef883855db80676f2214
Ali Gangji <cogent@gmail.com>
2012-01-06 16:36:38 -0800

    bug fix

--- script/generate/model/templates/base.php
+++ script/generate/model/templates/base.php
@@ -9,7 +9,7 @@ class <?= ucwords($name); ?>Model extends Table {
 		"<?= $column; ?>" => "<? foreach ($field['filters'] as $k => $v) { ?><? if ($count > 0) echo "  "; $count++ ?><?= $k; ?>:<?= $v; ?><? } ?>"<? } } echo "\n"; ?>
 	);
 
-	function onload() {<? foreach ($fields as $column => $field) { foreach ($field['filters'] as $k => $v) { if ($k == "references") { $v = explode(" ", $v); echo "\n"; ?>
+	function init() {<? foreach ($fields as $column => $field) { foreach ($field['filters'] as $k => $v) { if ($k == "references") { $v = explode(" ", $v); echo "\n"; ?>
 		$this->has_one("<?= $v[0]; ?>", "<?= $column; ?>");<? } } } ?><? foreach ($relations as $relation) { echo "\n"; ?>
 		$this->has_many("<?= $relation['model']; ?>", "<?= $relation['field']; ?>"<? if (!empty($relation['lookup'])) { ?>, "<?= $relation['lookup']; ?>", "<?= $relation['ref_field']; ?>"<? } ?>);<? } echo "\n"; ?>
 	}
