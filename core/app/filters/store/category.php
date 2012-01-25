bb41de415337494d5b984e4359e43c1ebc5d901d
Ali Gangji <cogent@gmail.com>
2012-01-25 11:41:49 -0800

    category filter update

--- core/app/filters/store/category.php
+++ core/app/filters/store/category.php
@@ -3,16 +3,15 @@ foreach($args as $field => $category) {
 		if(!is_numeric($fields[$field])) {
 			$errors[$field]['required'] = 'This field is required';
 		} else if($fields[$field] == -1) {
-			if(empty($fields[$field.'_new_category'])) {
-				$errors[$field.'_new_category'][] = 'This field is required';
-			} else {
+			if(!empty($fields[$field.'_new_category'])) {
 				$old_queue = $this->to_store;
 				$this->to_store = array();
 				$category_column_info = schema($name.".fields.".$field);
 				efault($category_column_info['taxonomy'], $name."_".$field);
 				sb('terms','create',array(
 					'term' => $fields[$field.'_new_category'],
-					'taxonomy' => $category_column_info['taxonomy']
+					'taxonomy' => $category_column_info['taxonomy'],
+					'parent' => 0
 				));
 				if(errors('terms[slug]')) $errors[$field.'_new_category'][] = "That $field already exists";
 				else {
