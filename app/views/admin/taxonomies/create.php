6536926626fa3d5d2b4c6dadd989aa8a79e8350f
Ali Gangji <cogent@gmail.com>
2012-01-24 20:06:45 -0800

    default term of Uncategorized

--- app/views/admin/taxonomies/create.php
+++ app/views/admin/taxonomies/create.php
@@ -1,8 +1,9 @@
 <h1>New Taxonomy</h1>	
 <?php
+	$_POST['terms']['term'] = "Uncategorized";
 	open_form("model:terms  action:create  uri:admin/taxonomies");
 	text("taxonomy");
-	text("term  label:Initial Term");
+	hidden("term");
 	$cancel_url = uri("admin/taxonomies");
 ?>
 <div class="field"><button class="left positive" type="submit">Save</button><button class="negative cancel button"<?php if (!empty($cancel_url)) { ?> onclick="window.location='<?= $cancel_url; ?>'"<?php } ?>>Cancel</button></div>
