0645c3e5753e662c4a5c52041d27685a7171f9b9
Ali Gangji <cogent@gmail.com>
2012-01-27 20:50:21 -0800

    upload filter

--- core/app/filters/store/upload.php
+++ core/app/filters/store/upload.php
@@ -2,14 +2,14 @@
 foreach($args as $field => $upload) {
 	$record = array("filename" => "", "mime_type" => "", "caption" => "$name $field");
 	if (!empty($fields[$field])) $records['id'] = $fields[$field];
-	$file = $_FILES[$upload];
+	$file = $_FILES[$field];
 	if (!empty($file['name'])) {
-		$file_errors = $this->get("files")->upload($record, $file);
-    if (empty($file_errors)) {
+		$this->get("files")->upload($record, $file);
+    if (if (!errors("files")) {
         $fields[$field] = (empty($record['id'])) ? $this->insert_id : $record['id'];
         unset($errors[$field]['required']);
     } else {
-        foreach($file_errors['filename'] as $type => $message) $errors[$field][$type] = $message;
+        foreach($errors("files[filename]", true) as $type => $message) $errors[$field][$type] = $message;
     }
 	}
 }
