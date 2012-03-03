ee28a4f75381c50d6c4a85c14b12542e65d955cc
Ali Gangji <cogent@gmail.com>
2012-03-02 16:18:59 -0800

    uris form update

--- core/app/forms/uris.php
+++ core/app/forms/uris.php
@@ -146,7 +146,7 @@ require(['dojo/query', 'dojo/domReady!'], function($) {
 	</div>
 	<?php
 	if ($action == "update") {
-		$count = query("blocks", "select:COUNT(*) as count  where:uris_id=?", array($_POST['uris']['id']));
+		$count = query("blocks", "select:COUNT(*) as count  where:uris_id=?  limit:1", array($_POST['uris']['id']));
 		if ($count['count'] == 0) store("blocks", "uris_id:".$_POST['uris']['id']."  type:text  region:content  position:1");
 		$containers = query("blocks", "where:uris_id=?  orderby:position ASC", array($_POST['uris']['id']));
 		$l = new form("model:new-block");
