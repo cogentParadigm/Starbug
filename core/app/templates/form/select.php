7e19f56c75c03b108e12b97ebcfc1e562c276bdd
Ali Gangji <cogent@gmail.com>
2012-03-09 23:03:49 -0800

    select template

--- core/app/templates/form/select.php
+++ core/app/templates/form/select.php
@@ -1,5 +1,5 @@
 <select<? foreach ($attributes as $key => $val) if (!empty($val) && !is_array($val) && !in_array($key, array("label", "field"))) echo ' '.$key.'="'.$val.'"'; ?>>
 	<? foreach ($options as $caption => $val) { ?>
-		<option value="<?= htmlentities($val); ?>"<? if ($val == $value) echo " selected=\"true\""; ?>><?= $caption; ?></option>
+		<option value="<?= htmlentities($val); ?>"<? if ((is_array($value) && in_array($val, $value)) || ($val == $value)) echo " selected=\"true\""; ?>><?= $caption; ?></option>
 	<? } ?>
 </select>
