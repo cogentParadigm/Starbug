41ca2e50786b93036db7eab54450d1541faa2a6e
Ali Gangji <cogent@gmail.com>
2012-06-01 23:17:57 -0700

    added module param to generator

--- script/generate.php
+++ script/generate.php
@@ -35,6 +35,8 @@ foreach ($argv as $i => $arg) {
 
 //SET VARS FOR GENERATOR 
 $model_name = $model;
+$app_dir = "app/";
+if (!empty($args['module'])) $app_dir = "modules/".$args['module'];
 $dirs = array(); $generate = array(); $copy = array();
 
 global $renderer;
--- script/generate/model/model.php
+++ script/generate/model/model.php
@@ -1,6 +1,6 @@
 <?php
 	$generate = array("base" => "var/models/".ucwords($model_name)."Model.php");
-	if (!file_exists(BASE_DIR."/app/models/".ucwords($model_name).".php")) {
-		$generate["model"] = "app/models/".ucwords($model_name).".php";
+	if (!file_exists(BASE_DIR.$app_dir."models/".ucwords($model_name).".php")) {
+		$generate["model"] = $app_dir."models/".ucwords($model_name).".php";
 	}
 ?>
