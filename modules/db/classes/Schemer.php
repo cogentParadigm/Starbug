dec7be119fe6f99612805f2e5a25a987c475d2d8
Ali Gangji <cogent@gmail.com>
2012-06-03 02:27:46 -0700

    allow for direct sql type specification

--- modules/db/classes/Schemer.php
+++ modules/db/classes/Schemer.php
@@ -386,7 +386,8 @@ class Schemer {
 	 */
 	function get_sql_type($field) {
 		$type = "varchar(".(isset($field['length'])?$field['length']:"64").")";
-		if ($field['type'] == 'password') $type = "varchar(32)";
+		if ($field['type'] == 'string') $type = "varchar(".(isset($field['length'])?$field['length']:"64").")";
+		else if ($field['type'] == 'password') $type = "varchar(32)";
 		else if (($field['type'] == 'text') || ($field['type'] == 'longtext')) $type = $field["type"];
 		else if ($field['type'] == 'int' || $field['type'] == 'category') $type = "int(".(isset($field['length'])?$field['length']:"11").")";
 		else if ($field['type'] == 'decimal') $type = "decimal(".$field['length'].")";
