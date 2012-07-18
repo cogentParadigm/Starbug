8b810f88a624b8e4de81674097414cf781186263
Ali Gangji <cogent@gmail.com>
2012-07-18 00:11:38 -0700

    references fix

--- core/app/filters/store/references.php
+++ core/app/filters/store/references.php
@@ -1,6 +1,6 @@
 <?php
 foreach($args as $field => $references) {
-	if (($inserting) && (empty($fields[$field]))) $fields[$field] = $this->get(reset(explode(" ", $references)))->insert_id;
+	if (($inserting) && (empty($fields[$field]))) $fields[$field] = $this->model(reset(explode(" ", $references)))->insert_id;
 	else if (!$storing) $on_store = true;
 }
 ?>
