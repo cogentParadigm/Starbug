817f9bb9989aec3b3e2d6b0f67bcf1b085c78bd8
Ali Gangji <cogent@gmail.com>
2012-11-24 06:10:51 -0800

    less-css support

--- modules/css/hooks/global.head.php
+++ modules/css/hooks/global.head.php
@@ -3,23 +3,32 @@
 			<!--<link rel="stylesheet" href="<?php echo uri("var/public/stylesheets/".$request->theme."-print.css"); ?>" type="text/css" media="print">-->
 			<!--[if IE]><link rel="stylesheet" href="<?php echo uri("var/public/stylesheets/".$request->theme."-ie.css"); ?>" type="text/css" media="screen, projection"><![endif]-->
 	<?php } else { ?>
-			<?php
-				$styles = theme("styles", $request->theme);
-				efault($styles['plugins'], array());
-				efault($styles['screen'], array());
-				efault($styles['print'], array());
-				efault($styles['ie'], array());
-				efault($styles['blueprint'], false);
-			?>
+	
+		<?php
+			$styles = theme("styles", $request->theme);
+			efault($styles['plugins'], array());
+			efault($styles['screen'], array());
+			efault($styles['print'], array());
+			efault($styles['ie'], array());
+			efault($styles['blueprint'], false);
+			efault($styles['less'], false);
+		?>
+			
 		<?php if ($styles['blueprint']) { ?>
 			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/reset.css"); ?>" type="text/css" media="screen, projection">
 			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/typography.css"); ?>" type="text/css" media="screen, projection">
 			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/forms.css"); ?>" type="text/css" media="screen, projection">
 			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/grid.css"); ?>" type="text/css" media="screen, projection">
+			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/print.css"); ?>" type="text/css" media="print">
+			<!--[if IE]><link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/ie.css"); ?>" type="text/css" media="screen, projection"><![endif]-->
 		<?php } ?>
-		<?php if (file_exists(BASE_DIR."/app/themes/".$request->theme."/public/stylesheets/custom-screen.css")) { ?>
+		
+		<?php if ($styles['less'] && file_exists(BASE_DIR."/app/themes/".$request->theme."/public/stylesheets/custom-screen.less")) { ?>
+			<link rel="stylesheet/less" href="<?php echo uri("app/themes/".$request->theme."/public/stylesheets/custom-screen.less"); ?>" type="text/css" media="screen, projection">
+		<?php } else if (file_exists(BASE_DIR."/app/themes/".$request->theme."/public/stylesheets/custom-screen.css")) { ?>
 			<link rel="stylesheet" href="<?php echo uri("app/themes/".$request->theme."/public/stylesheets/custom-screen.css"); ?>" type="text/css" media="screen, projection">
 		<?php } ?>
+
 		<?php if (file_exists(BASE_DIR."/app/themes/".$request->theme."/public/stylesheets/custom-print.css")) { ?>
 			<link rel="stylesheet" href="<?php echo uri("app/themes/".$request->theme."/public/stylesheets/custom-print.css"); ?>" type="text/css" media="print">
 		<?php } ?>
@@ -29,14 +38,8 @@
 		<?php foreach ($styles['screen'] as $screen) { ?>
 			<link rel="stylesheet" href="<?php echo uri("app/themes/".$request->theme."/public/stylesheets/$screen"); ?>" type="text/css" media="screen, projection">
 		<?php } ?>
-		<?php if ($styles['blueprint']) { ?>
-			<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/print.css"); ?>" type="text/css" media="print">
-		<?php } ?>
 		<?php foreach ($styles['print'] as $print) { ?>
 			<link rel="stylesheet" href="<?php echo uri("app/themes/".$request->theme."/public/styesheets/$print"); ?>" type="text/css" media="print">
-		<?php } ?>
-		<?php if ($styles['blueprint']) { ?>
-			<!--[if IE]><link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/src/ie.css"); ?>" type="text/css" media="screen, projection"><![endif]-->
 		<?php } ?>
 		<?php foreach ($styles['ie'] as $ie) { ?>
 			<!--[if IE]><link rel="stylesheet" href="<?php echo uri("app/themes/".$request->theme."/public/stylesheets/$ie"); ?>" type="text/css" media="screen, projection"><![endif]-->
@@ -45,5 +48,8 @@
 				<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/plugins/$plugin/screen.css"); ?>" type="text/css" media="screen, projection">
 				<link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/plugins/$plugin/print.css"); ?>" type="text/css" media="print">
 				<!--[if IE]><link rel="stylesheet" href="<?php echo uri("core/app/public/stylesheets/plugins/$plugin/ie.css"); ?>" type="text/css" media="screen, projection"><![endif]-->
+		<?php } ?>
+		<?php if ($styles['less']) { ?>
+			<script src="<?php echo uri("core/app/public/js/less-1.3.1.min.js"); ?>" type="text/javascript"></script>
 		<?php } ?>
 	<?php } ?>
--- script/generate/css/css.php
+++ script/generate/css/css.php
@@ -2,14 +2,27 @@
 	$sb->import("core/lib/CSSParser");
 	$themes = config("themes");
 	foreach ($themes as $name) {
+		
 		$conf = theme("styles", $name);
-		$screen = new CSSParser(
-			BASE_DIR."/core/app/public/stylesheets/src/reset.css",
-			BASE_DIR."/core/app/public/stylesheets/src/typography.css",
-			BASE_DIR."/core/app/public/stylesheets/src/forms.css",
-			BASE_DIR."/core/app/public/stylesheets/src/grid.css",
-			BASE_DIR."/var/public/stylesheets/$name-screen.css"
-		);
+		efault($conf['blueprint'], false);
+		efault($conf['less'], false);
+		
+		/******************SCREEN********************/
+		if ($conf['blueprint']) {
+			$screen = new CSSParser(
+				BASE_DIR."/core/app/public/stylesheets/src/reset.css",
+				BASE_DIR."/core/app/public/stylesheets/src/typography.css",
+				BASE_DIR."/core/app/public/stylesheets/src/forms.css",
+				BASE_DIR."/core/app/public/stylesheets/src/grid.css",
+				BASE_DIR."/var/public/stylesheets/$name-screen.css"
+			);
+		} else {
+			$screen = new CSSParser(BASE_DIR."/var/public/stylesheets/$name-screen.css");			
+		}
+		//compile custom-screen.less
+		if ($conf['less'] && file_exists(BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-screen.less")) {
+			exec("lessc ".BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-screen.less ".BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-screen.css");
+		}
 		//add custom-screen.css
 		if (file_exists(BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-screen.css")) $screen->add_file(BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-screen.css", "$name custom-screen.css");
 		//add additional screen styles
@@ -17,12 +30,17 @@
 		//add plugins
 		if (!empty($conf['plugins'])) foreach ($conf['plugins'] as $plugin) $screen->add_plugin($plugin);
 		$screen->parse();
-		if (!empty($conf['classes'])) $screen->add_semantic_classes($conf['classes']);
 		$screen->write();
-		$print = new CSSParser(
-			BASE_DIR."/core/app/public/stylesheets/src/print.css",
-			BASE_DIR."/var/public/stylesheets/$name-print.css"
-		);
+		
+		/******************PRINT********************/
+		if ($conf['blueprint']) {
+			$print = new CSSParser(
+				BASE_DIR."/core/app/public/stylesheets/src/print.css",
+				BASE_DIR."/var/public/stylesheets/$name-print.css"
+			);
+		} else {
+			$print = new CSSParser(BASE_DIR."/var/public/stylesheets/$name-print.css");
+		}
 		//add custom-print.css
 		if (file_exists(BASE_DIR."app/themes/".$name."/public/stylesheets/custom-print.css")) $print->add_file(BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-print.css");
 		//add additional print styles
@@ -30,12 +48,17 @@
 		//add plugins
 		if (!empty($conf['plugins'])) foreach ($conf['plugins'] as $plugin) $print->add_plugin($plugin);
 		$print->parse();
-		if (!empty($conf['classes'])) $screen->add_semantic_classes($conf['classes']);
 		$print->write();
-		$ie = new CSSParser(
-			BASE_DIR."/core/app/public/stylesheets/src/ie.css",
-			BASE_DIR."/var/public/stylesheets/$name-ie.css"
-		);
+
+		/******************IE********************/
+		if ($conf['blueprint']) {
+			$ie = new CSSParser(
+				BASE_DIR."/core/app/public/stylesheets/src/ie.css",
+				BASE_DIR."/var/public/stylesheets/$name-ie.css"
+			);
+		} else {
+			$ie = new CSSParser(BASE_DIR."/var/public/stylesheets/$name-ie.css");
+		}
 		//add custom-ie.css
 		if (file_exists(BASE_DIR."app/themes/".$name."/public/stylesheets/custom-ie.css")) $ie->add_file(BASE_DIR."/app/themes/".$name."/public/stylesheets/custom-ie.css");
 		//add additional ie styles
@@ -43,7 +66,6 @@
 		//add plugins
 		if (!empty($conf['plugins'])) foreach ($conf['plugins'] as $plugin) $ie->add_plugin($plugin);
 		$ie->parse();
-		if (!empty($conf['classes'])) $ie->add_semantic_classes($conf['classes']);
 		$ie->write();
 	}
 ?>
