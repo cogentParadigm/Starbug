80154946567d6ac7ae714493191a941746dcfd5d
Ali Gangji <cogent@gmail.com>
2012-12-17 14:10:11 -0800

    search fix

--- modules/db/filters/query/search.php
+++ modules/db/filters/query/search.php
@@ -5,7 +5,7 @@ if (!empty($args['keywords'])) {
 	$preliminary_search_fields = explode(",", $args['search']);
 	$search_fields = array();
 	foreach ($preliminary_search_fields as $s => $f) {
-		$col_name = (false === strpos($f, ".")) ? $f : reset(explode(".", $f));
+		$col_name = (false === strpos($f, ".")) ? $f : end(explode(".", $f));
 		if (!$this->has($schema['fields'][$col_name]['type'])) $search_fields[] = $col_name;
 	}
 	//append search conditions
