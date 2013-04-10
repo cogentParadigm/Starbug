0d5cee42c367c4abfe95a3a972cb7b481bfe0b10
Ali Gangji <cogent@gmail.com>
2013-04-10 04:06:42 -0700

    generate hmac secret when generating host

--- core/app/script/generate/host/host.php
+++ core/app/script/generate/host/host.php
@@ -1,6 +1,6 @@
 <?php
 	if (!defined("BASE_DIR")) define("BASE_DIR", str_replace("core/app/script/generate/host", "", dirname(__FILE__)));
-	$conf = json_decode(file_get_contents(BASE_DIR."/etc/host.json"), true);
+	$conf = json_decode(str_replace('%HMAC_KEY%', md5(uniqid(rand(), TRUE)), file_get_contents(BASE_DIR."/etc/host.json")), true);
 	$host = (file_exists(BASE_DIR."/etc/Host.php")) ? file_get_contents(BASE_DIR."/etc/Host.php") : $migration = file_get_contents(BASE_DIR."/core/app/script/generate/host/BlankHost.php");
 	foreach ($conf as $name => $value) {
 		if (false === strpos($host, "const ".$name)) {
