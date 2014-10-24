e195c7d439a041cf9e18fd3294dbcc3c74e733e7
Ali Gangji <cogent@gmail.com>
2014-10-23 22:34:19 -0700

    menu and terms UI fixes

--- app/models/Terms.php
+++ app/models/Terms.php
@@ -66,17 +66,18 @@ class Terms {
 		//layout
 		$display->layout->add("top  left:div.col-md-9  right:div.col-md-3", "bottom  tabs:div.col-sm-12");
 		$display->layout->put("tabs", 'div[data-dojo-type="dijit/layout/TabContainer"][data-dojo-props="doLayout:false, tabPosition:\'left-h\'"][style="width:100%;height:100%"]', '', 'tc');
-		$display->layout->put("tc", 'div[data-dojo-type="dijit/layout/ContentPane"][title="URL path"]'.((empty($_GET['tab'])) ? '[data-dojo-props="selected:true"]' : '').'[style="min-height:200px"]', '', 'path');
-		$display->layout->put("tc", 'div[data-dojo-type="dijit/layout/ContentPane"][title="Breadcrumbs"]'.(($_GET['tab'] === "breadcrumbs") ? '[data-dojo-props="selected:true"]' : '').'[style="min-height:200px"]', '', 'breadcrumbs');
+		$display->layout->put("tc", 'div[data-dojo-type="dijit/layout/ContentPane"][title="Machine Name"]'.((empty($_GET['tab'])) ? '[data-dojo-props="selected:true"]' : '').'[style="min-height:200px"]', '', 'path');
+		$display->layout->put("tc", 'div[data-dojo-type="dijit/layout/ContentPane"][title="Parent"]'.(($_GET['tab'] === "breadcrumbs") ? '[data-dojo-props="selected:true"]' : '').'[style="min-height:200px"]', '', 'breadcrumbs');
 		//left
 		$display->add("term  pane:left");
-		$display->add("taxonomy  pane:left  default:".filter_string($_GET['taxonomy']));
-		$display->add("description  pane:left");
+		if (!empty($_GET['new'])) $display->add("taxonomy  pane:left  input_type:text");
+		else $display->add("taxonomy  pane:left  default:".filter_string($_GET['taxonomy']));
+		$display->add("description  pane:left  class:rich-text");
 		//$display->add("blocks  input_type:blocks  pane:left");
 		$display->add("images  pane:left  input_type:file_select  size:0");
 		//right
 		$display->add("groups  taxonomy:groups  input_type:multiple_category_select  pane:right");
-		$display->add("slug  label:URL path  info:Leave empty to generate automatically  pane:path");
+		$display->add("slug  label:Machine Name  info:Leave empty to generate automatically  pane:path");
 		//$display->add("breadcrumb  label:Breadcrumbs Title  style:width:100%  pane:breadcrumbs");
 		$display->add("parent  info:Start typing the title of the page and autocomplete results will display  input_type:autocomplete  pane:breadcrumbs");
 	}
