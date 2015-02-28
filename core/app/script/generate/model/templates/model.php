b39e609be917676a6676ffb4dcf1bbe5ac26bfda
Ali Gangji <cogent@gmail.com>
2015-02-28 07:42:14 -0800

    updated template for generated models

--- core/app/script/generate/model/templates/model.php
+++ core/app/script/generate/model/templates/model.php
@@ -9,5 +9,31 @@ class <?= ucwords($name); ?> {
 		$this->store($<?= $singular; ?>);
 	}
 
+  /******************************************************************
+	 * Query functions
+	 *****************************************************************/
+
+  function query_admin($query, &$ops) {
+    $query = parent::query_admin($query, $ops);
+    return $query;
+  }
+
+  function query_filters($action, $query, $ops) {
+      if (!logged_in("root") && !logged_in("admin")) $query->action("read");
+      return $query;
+  }
+
+	/******************************************************************
+	 * Display functions
+	 *****************************************************************/
+
+	function display_admin($display, $ops) {
+	<?php if (!empty($base)) { ?>
+    sb($this->base)->display_admin($display, $ops);
+  <?php } else { ?>
+    $display->add("id");
+  <?php } ?>
+	}
+
 }
 <?= '?>'; ?>
