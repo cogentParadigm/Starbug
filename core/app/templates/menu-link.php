4d4152a2c9894cbf2e5e98fe97ca09e0a2b07d51
Ali Gangji <cogent@gmail.com>
2015-03-01 22:08:27 -0800

    updated menu link template

--- core/app/templates/menu-link.php
+++ core/app/templates/menu-link.php
@@ -1,6 +1,7 @@
 <?php
 	$attributes = $class = $link_attributes = array();
 	$link_text = "";
+	$active = false;
 
 	//add first class for first item in list
 	if ($link['position'] == 0) $class[] = "first";
@@ -19,7 +20,16 @@
 	if (!empty($link['term'])) $link['content'] = $link['term'];
 
 	//href override
-	if (!empty($link['href'])) $link_attributes['href'] = (0 === strpos($link['href'], "http")) ? $link['href'] : uri($link['href']);
+	if (!empty($link['href'])) {
+		$absolute = (0 === strpos($link['href'], "http"));
+		$link_attributes['href'] = $absolute ? $link['href'] : uri($link['href']);
+		if (!$absolute) {
+			$parts = explode("/", $link['href']);
+			$active = true;
+			foreach ($parts as $idx => $part) if (request()->uri[$idx] !== $part) $active = false;
+			if ($active) $class[] = "active";
+		}
+	}
 
 	//content override
 	if (!empty($link['content'])) $link_text = $link['content'];
@@ -31,11 +41,15 @@
 	if (!empty($link['children'])) {
 		$class[] = "dropdown";
 		$class[] = "clearfix";
-		//$attributes['data-dojo-type'] = "bootstrap/Dropdown";
+		if ($active) {
+			$class[] = "open";
+			unset($link_attributes['href']);
+		} else {
+			$link_attributes['data-toggle'] = "dropdown";
+			$link_attributes['role'] = "button";
+			$link_attributes['data-target'] = "#";
+		}
 		$link_attributes['class'] = "dropdown-toggle";
-		$link_attributes['data-toggle'] = "dropdown";
-		$link_attributes['role'] = "button";
-		$link_attributes['data-target'] = "#";
 		$link_text .= '<i class="fa chevron pull-right"></i>';
 	}
 
