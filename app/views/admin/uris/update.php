7930976c061ea54bce49b1169387b4c8ae086fb4
Ali Gangji <cogent@gmail.com>
2015-03-15 12:36:58 -0700

    additional cleanup of old files and functions

--- app/views/admin/uris/update.php
+++ app/views/admin/uris/update.php
@@ -1,13 +1,13 @@
 <?php
 	$record = (is_numeric($id)) ? get("uris", $id) : get("uris", array(), array("sort" => array("modified" => -1), "limit" => 1));
 	$id = $record['id'];
-	efault($_POST["uris"], array());
+	if (empty($_POST['uris'])) $_POST['uris'] = array();
 	if (empty($_POST["uris"]["categories"])) {
 		$_POST["uris"]["categories"] = array();
 		$cats = get("uris_categories", array("uris_id" => $id));
 		foreach ($cats as $c) $_POST["uris"]["categories"][] = $c['terms_id'];
 	}
-	foreach ($record as $k => $v) dfault($_POST["uris"][$k], $v);
+	foreach ($record as $k => $v) if (!isset($_POST['uris'][$k])) $_POST['uris'][$k] = $v;
 	if (!empty($_POST["uris"]['id'])) $id = $_POST["uris"]['id'];
 	$this->assign("id", $id);
 	$this->assign("action", "update");
