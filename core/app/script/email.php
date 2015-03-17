d5dbe2576d04bf44965ccc404aaeaa58608a08a1
Ali Gangji <cogent@gmail.com>
2015-03-16 21:20:16 -0700

    removing old files

--- core/app/script/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-import("mailer");
-$mailer = new mailer();
-$next = array_shift($argv);
-include(BASE_DIR."/core/app/script/email/$next/$next.php");
-$result = $mailer->Send();
-echo $result;
-?>
