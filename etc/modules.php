d6b936a8b4d316e775254d2051d2566440969d94
Ali Gangji <cogent@gmail.com>
2015-03-18 21:38:40 -0700

    missing file

---
+++ etc/modules.php
@@ -0,0 +1,13 @@
+<?php
+$modules = array(
+	"core" => "core/app",
+	"db" => "modules/db",
+	"users" => "modules/users",
+	"emails" => "modules/emails",
+	"files" => "modules/files",
+	"comments" => "modules/comments",
+	"css" => "modules/css",
+	"js" => "modules/js",
+	"theme" => "app/themes/starbug-1",
+	"app" => "app"
+);
