9ea295983292dddccf629ea2a2c31f6fefbed605
Ali Gangji <cogent@gmail.com>
2015-04-11 21:56:25 -0700

    fix for file select handling

--- core/app/templates/form/input.php
+++ core/app/templates/form/input.php
@@ -4,12 +4,8 @@
 		$column_info = column_info($display->model, $field);
 		$records = array();
 		if (!empty($value)) {
-			if ($column_info['type'] == "files") {
-				$table = $column_info['entity']."_".$field;
-				$records = query($table)->condition($table.".".$field."_id", $value)->select($field."_id.*")->sort($table.".position")->all();
-			} else {
-				$records = query("files")->condition("id", $value)->all();
-			}
+			if (!is_array($value)) $value = explode(",", preg_replace("/[,\s]+/", ",", $value));
+			$records = query("files")->condition("id", $value)->sort("FIELD(id, '".implode("','", $value)."')")->all();
 		}
 		if (count($records)) $files = str_replace('"', "'", json_encode($records));
 		$attrs = 'data-dojo-type="starbug/form/FileSelect" data-dojo-props="input_name:\''.$attributes['name'].'\', files:'.$files;
