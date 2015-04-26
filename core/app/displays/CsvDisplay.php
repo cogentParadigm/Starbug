007f148f120667c2e8888c6c6b22965d4e16698d
Ali Gangji <cogent@gmail.com>
2015-04-26 15:48:34 -0700

    csv display fix

--- core/app/displays/CsvDisplay.php
+++ core/app/displays/CsvDisplay.php
@@ -4,7 +4,7 @@ class CsvDisplay extends ItemDisplay {
 	function build_display($options) {
 		$this->model = $options['model'];
 		$this->action = $options['action'];
-		sb($this->model)->build_display($display);
+		sb($this->model)->build_display($this);
 	}
 	function query($options=null, $model="") {
 		if (isset($this->options['data'])) $this->items = $this->options['data'];
