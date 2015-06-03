5375c9d3c57fa312fcade6d0dd86269f40138dc6
Ali Gangji <cogent@gmail.com>
2015-06-03 07:35:51 -0700

    missing QueryQueue class

---
+++ modules/db/src/QueryQueue.php
@@ -0,0 +1,47 @@
+<?php
+# Copyright (C) 2008-2010 Ali Gangji
+# Distributed under the terms of the GNU General Public License v3
+/**
+ * This file is part of StarbugPHP
+ * @file modules/db/src/QueryQueue.php
+ * @author Ali Gangji <ali@neonrain.com>
+ * @ingroup db
+ */
+/**
+ * usage:
+ * $queue = new QueryQueue();
+ * $queue->push($query);
+ * $queue->push($query2);
+ * $queue->execute();
+ * @ingroup db
+ */
+
+class QueryQueue extends SplQueue {
+	public $active;
+	/**
+	 * Push a query or queries onto the end of the queue
+	 * @param query $query the query to push onto the queue
+	 */
+	function push($query) {
+		$query->validate();
+		parent::push($query);
+	}
+	/**
+	 * Prepend a query or queries to the front of the queue
+	 * @param query $query the query to prepend to the queue
+	 */
+	function unshift($query) {
+		$query->validate();
+		parent::unshift($query);
+	}
+	/**
+	 * Execute the queue of queries
+	 */
+	function execute() {
+		while (!$this->isEmpty()) {
+			$this->active = $this->shift();
+			$this->active->execute();
+		}
+	}
+}
+?>
