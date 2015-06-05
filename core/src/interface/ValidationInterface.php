ee5c026fb1cb678787e135e9b4580f0f67edb20c
Ali Gangji <cogent@gmail.com>
2015-06-05 05:25:31 -0700

    Logger implementation, ValidationInterface and Validation to replace global errors API

--- core/src/interface/ValidationInterface.php
+++ core/src/interface/ValidationInterface.php
@@ -3,39 +3,20 @@
 # Distributed under the terms of the GNU General Public License v3
 /**
  * This file is part of StarbugPHP
- * @file core/src/interface/LoggerInterface.php
+ * @file core/src/interface/ValidationInterface.php
  * @author Ali Gangji <ali@neonrain.com>
  */
 /**
- * validation logger
+ * interface for getting/setting validation errors
  */
-class ValidationLogger {
-	public $errors = array();
-	public $scope = "global";
-	protected $logger;
-	protected $request;
-	function __construct(LoggerInterface $logger, RequestInterface $request) {
-		$this->logger = $logger;
-		$this->request = $request;
-		$this->logger->type = "validation";
-	}
+interface ValidationInterface {
 	/**
 	* fetch any data validation errors
 	* @ingroup errors
 	* @param string $key the model or field to get errors for. If this is empty all errors will be returned. To get errors on a model pass the name of the model. To get errors on a field use array notation such as 'users[first_name]'
 	* @return array errors indexed by model and field, empty if no errors
 	*/
-	function errors($key="", $values=false) {
-		if (is_bool($key)) {
-			$values = $key;
-			$key = "";
-		}
-		$parts = explode("[", $key);
-		$errors = $this->errors;
-		if (!empty($key)) foreach ($parts as $p) $errors = $errors[rtrim($p, ']')];
-		if ($values) return $errors;
-		else return (!empty($errors));
-	}
+	function errors($key = "", $values = false);
 	/**
 	* set validation error
 	* @ingroup errors
@@ -43,22 +24,14 @@ class ValidationLogger {
 	* @param $field the field name
 	* @param $error the error message
 	*/
-	function error($error, $field="global", $model="") {
-		if (empty($model)) $model = $this->scope;
-		$this->errors[$model][$field][] = $error;
-		$this->logger->error("{model}::{action} - {field}:{message}", array("model" => $model, "action" => $this->request->data['action'][$model], "field" => $field, "message" => $error));
-	}
+	function error($error, $field = "global", $model = "");
 	/**
 	* get or set the error scope
 	* @ingroup errors
 	* @param $value if not null, the value to set the scope to
 	* @return the active scope. if a value is passed, this will return the scope that is active before setting the value
 	*/
-	function error_scope($value=null) {
-		$scope = $this->scope;
-		if ($value != null) $this->scope = $value;
-		return $scope;
-	}
+	function error_scope($value = null);
 	/**
 	* check that an action was called and no errors occurred
 	* @ingroup errors
@@ -66,10 +39,7 @@ class ValidationLogger {
 	* @param string $action the function name
 	* @return bool true if the function was called without returning errors
 	*/
-	function success($model, $action) {
-		return (($this->request->data['action'][$model] == $action) && (empty($this->errors[$model])));
-	}
-
+	function success($model, $action);
 	/**
 	* check if an action was called and errors were produced
 	* @ingroup errors
@@ -77,7 +47,5 @@ class ValidationLogger {
 	* @param string $action the function name
 	* @return bool true if the function was called and produced errors
 	*/
-	function failure($model, $action) {
-		return (($this->request->data['action'][$model] == $action) && (!empty($this->errors[$model])));
-	}
+	function failure($model, $action);
 }
