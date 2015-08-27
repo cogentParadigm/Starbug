b77c13eed363cadcc0c853cef168d12b682ed3f6
Ali Gangji <cogent@gmail.com>
2015-08-27 01:18:36 -0700

    run button in imports grid

--- core/app/displays/ImportsGrid.php
+++ core/app/displays/ImportsGrid.php
@@ -4,6 +4,7 @@ class ImportsGrid extends GridDisplay {
 	public $action = "admin";
 	function build_display($options) {
 		$this->add("name", "model", "created", "modified  label:Last Modified");
+		$this->add("row_options  plugin:starbug.grid.columns.import_options");
 	}
 }
 ?>
