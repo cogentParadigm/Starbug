5a96b71038da8dbd8067fb977693c55ebd7b5987
Ali Gangji <cogent@gmail.com>
2015-09-01 07:17:13 -0700

    intl module: sort languages by name

--- modules/intl/models/Languages.php
+++ modules/intl/models/Languages.php
@@ -9,12 +9,18 @@ class Languages {
 	 * Query functions
 	 *****************************************************************/
 
+	function query_admin($query, &$ops) {
+		$query->sort("languages.name");
+		return $query;
+	}
+
 	function query_select($query, &$ops) {
 		if (!empty($ops['id'])) {
 			$query->condition($query->model.".id", explode(",", $ops['id']));
 		}
 		$query->select("languages.id");
 		$query->select("languages.name as label");
+		$query->sort("languages.name");
 		return $query;
 	}
 }
