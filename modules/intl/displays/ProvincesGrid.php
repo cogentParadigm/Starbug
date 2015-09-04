859b1c1e5222bf3ddf1a0bd327ceee1a2d7cb604
Ali Gangji <cogent@gmail.com>
2015-09-03 18:18:49 -0700

    updates

--- modules/intl/displays/ProvincesGrid.php
+++ modules/intl/displays/ProvincesGrid.php
@@ -3,7 +3,6 @@ class ProvincesGrid extends GridDisplay {
 	public $model = "provinces";
 	public $action = "admin";
 	function build_display($options) {
-		$this->add("countries_id");
 		$this->add("name");
 		$this->add("code");
 	}
