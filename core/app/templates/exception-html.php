35c6f4e2a039012653f9aced55a0fc21cf83ec1e
Ali Gangji <ali@neonrain.com>
2016-02-25 12:13:02 -0700

    updated cache headers

--- core/app/templates/exception-html.php
+++ core/app/templates/exception-html.php
@@ -3,6 +3,8 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<meta mttp-equiv="Cache-Control" content="no-cache,max-age=0"/>
+	<meta mttp-equiv="Pragma" content="no-cache"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <title><?php echo $data['type']; ?></title>
 
