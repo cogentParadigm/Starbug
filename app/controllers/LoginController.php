38aca5b67cf9cfd294a63c299d2256562e19b87a
Ali Gangji <ali@neonrain.com>
2016-04-03 15:53:42 -0600

    login controller bug fix

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -2,13 +2,15 @@
 namespace Starbug\App;
 use Starbug\Core\Controller;
 use Starbug\Core\SessionHandlerInterface;
+use Starbug\Core\IdentityInterface;
 class LoginController extends Controller {
-	public function __construct(SessionHandlerInterface $session) {
+	public function __construct(SessionHandlerInterface $session, IdentityInterface $user) {
 		$this->session = $session;
+		$this->user = $user;
 	}
 	function default_action() {
-		if ($this->session->loggedIn()) {
-			if ($this->session->loggedIn('admin') || $this->session->loggedIn('root')) $this->redirect('admin');
+		if ($this->user->loggedIn()) {
+			if ($this->user->loggedIn('admin') || $this->user->loggedIn('root')) $this->redirect('admin');
 			else $this->redirect('');
 		} else {
 			$this->render("login");
