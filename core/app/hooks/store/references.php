9b15dde65d06c4911b3107ede33bd0247a581a26
Ali Gangji <ali@neonrain.com>
2016-09-10 11:37:01 -0600

    support record array for references hook

--- core/app/hooks/store/references.php
+++ core/app/hooks/store/references.php
@@ -2,12 +2,22 @@
 namespace Starbug\Core;
 class hook_store_references extends QueryHook {
 	protected $replace = false;
-	function __construct(ModelFactoryInterface $models) {
+	function __construct(ModelFactoryInterface $models, DatabaseInterface $db) {
 		$this->models = $models;
+		$this->db = $db;
 	}
 	function validate(&$query, $key, $value, $column, $argument) {
 		if ($query->unvalidated[$key] === "") {
 			$this->replace = true;
+		} else if (is_array($value)) {
+			$model = reset(explode(" ", $argument));
+			$instance = $this->models->get($model);
+			$instance->post("create", $value);
+			if ($instance->success("create")) {
+				$value = empty($value["id"]) ? $instance->insert_id : $value["id"];
+			} else {
+				$errors = $this->db->errors[$model];
+			}
 		}
 		return $value;
 	}
