b5914be5777325fe70fca25af99ddb1ad4e4e7ba
Ali Gangji <ali@neonrain.com>
2016-10-06 13:09:00 -0600

    override post function on product lines to grant access based on order ID

--- modules/payment/models/Product_lines.php
+++ modules/payment/models/Product_lines.php
@@ -27,23 +27,8 @@ class Product_lines extends Product_linesModel {
 		}
 	}
 	public function post($action, $data = array()) {
-		$this->action = $action;
-		if ($this->user->loggedIn("admin") || $this->user->loggedIn("root")) return true;
-		else if (isset($data['id'])) {
-			$order = $this->cart->get("id");
-			$permits = $this->db->query($this->type)
-										->condition($this->type.".id", $data['id'])
-										->condition($this->type.".orders_id", $order)->one();
-		} else {
-			$permits = $this->db->query("permits")->action($action, $this->type)->one();
-		}
-		if ($permits) {
-			$this->$action($data);
-			return true;
-		} else {
-			$this->error("Access Denied");
-			return false;
-		}
+		if ($action == "update" || $action == "delete") return true;
+		else return parent::post($action, $data);
 	}
 }
 ?>
