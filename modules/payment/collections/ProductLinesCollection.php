0b4ee3b097ced66a057ec0a804c48c0e64cad6fd
Ali Gangji <ali@neonrain.com>
2016-10-06 19:17:47 -0600

    format product lines for recurring payments

--- modules/payment/collections/ProductLinesCollection.php
+++ modules/payment/collections/ProductLinesCollection.php
@@ -19,6 +19,20 @@ class ProductLinesCollection extends Collection {
 			$item['total'] = $item['price'] * $item['qty'];
 			$item['total_formatted'] = money_format('%.2n', $item['total']/100);
 			$item['price_formatted'] = money_format('%.2n', $item['price']/100);
+			if ($item["recurring"]) {
+				$unit = $item["unit"];
+				$interval = $item["interval"];
+				$phrase = "";
+				if ($unit == "days" && $interval == 365) {
+					$phrase = "year";
+				} else if ($interval == 1) {
+					$phrase = substr($unit, 0, 1);
+				} else {
+					$phrase = $interval." ".$unit;
+				}
+				$item["price_formatted"] .= "/".$phrase;
+				$item["total_formatted"] .= "/".$phrase;
+			}
 			$rows[$idx] = $item;
 		}
 		return $rows;
