5821ba00c7e263893aaa082d7cdcc44dc68da65d
Ali Gangji <ali@neonrain.com>
2016-10-08 13:36:11 -0600

    missing helper for formatting prices

---
+++ modules/payment/helpers/PriceFormatterHelper.php
@@ -0,0 +1,11 @@
+<?php
+namespace Starbug\Payment;
+class PriceFormatterHelper {
+	public function __construct(PriceFormatterInterface $target) {
+		$this->target = $target;
+	}
+	public function helper() {
+		return $this->target;
+	}
+}
+?>
