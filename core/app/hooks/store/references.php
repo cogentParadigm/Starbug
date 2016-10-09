0f290443df4a982761d9f2265bca171a72fc9ac9
Ali Gangji <ali@neonrain.com>
2016-10-09 12:11:12 -0600

    moved nested object storage from references hook to new operation hook

--- core/app/hooks/store/references.php
+++ core/app/hooks/store/references.php
@@ -2,26 +2,12 @@
 namespace Starbug\Core;
 class hook_store_references extends QueryHook {
 	protected $replace = false;
-	function __construct(ModelFactoryInterface $models, DatabaseInterface $db) {
+	function __construct(ModelFactoryInterface $models) {
 		$this->models = $models;
-		$this->db = $db;
 	}
 	function validate(&$query, $key, $value, $column, $argument) {
 		if ($query->unvalidated[$key] === "") {
 			$this->replace = true;
-		} else if (is_array($value)) {
-			$model = reset(explode(" ", $argument));
-			$instance = $this->models->get($model);
-			$instance->post("create", $value);
-			if ($instance->success("create")) {
-				$value = empty($value["id"]) ? $instance->insert_id : $value["id"];
-			} else {
-				$errors = $this->db->errors[$model];
-				unset($this->db->errors[$model]);
-				foreach ($errors as $field => $e) {
-					$this->db->errors[$query->model][$key.".".$field] = $e;
-				}
-			}
 		}
 		return $value;
 	}
