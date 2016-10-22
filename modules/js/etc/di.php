0ecaad3bfeb4165165e66ae6c2c4a595ba9b0db7
Ali Gangji <ali@neonrain.com>
2016-10-22 14:29:30 -0600

    modularized dojo loader and build configuration

---
+++ modules/js/etc/di.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'Starbug\Js\DojoBuildGenerateCommand' => DI\object()->constructorParameter('base_directory', DI\get('base_directory'))
+];
+?>
--- modules/js/templates/hook/global.head.php
+++ modules/js/templates/hook/global.head.php
@@ -1,46 +1,40 @@
-		<script type="text/javascript">
-			var dojoConfig = {
-				async:true,
-				parseOnLoad:true,
-				serverTime:'<?php echo date("Y-m-d H:i:s"); ?>'
-			};
-		</script>
-	<?php if (Etc::ENVIRONMENT == "production") { ?>
-		<!--[if lt IE 9]>
-			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-			<script type="text/javascript" src="<?php echo $this->url->build("libraries/release/dojo/dojo/dojo.js"); ?>"></script>
-			<?php $response->js("dojo/parser"); ?>
-	<?php } else { ?>
-			<?php
-				$profile = file_get_contents(BASE_DIR."/etc/dojo.profile.js");
-				$profile = explode("\n", substr($profile, 15));
-				foreach ($profile as $i => $p) if (0 === strpos(trim($p), "//")) unset($profile[$i]);
-				$profile = str_replace(array("dependencies:", "layers:", "name:", "prefixes:", "'"), array('"dependencies":', '"layers":', '"name":', '"prefixes":', '"'), implode(" ", $profile));
-				$profile = json_decode($profile, true);
-				$paths = "";
-				foreach ($profile['prefixes'] as $p) $paths .= "'$p[0]':'$p[1]', ";
-			?>
-			<script type="text/javascript" src="<?php echo $this->url->build("libraries/dojo/dojo.js"); ?>"></script>
-			<script type="text/javascript">
-				require({
-						packages: [<?php foreach ($profile['prefixes'] as $idx => $p) { if ($idx > 0) echo ','; echo "\n"; ?>
-								{ name: '<?php echo $p[0]; ?>', location: '<?php echo $p[1]; ?>' }<?php } echo "\n"; ?>
-						]
-				}, [
-					<?php foreach ($profile['layers'] as $lidx => $l) { foreach ($l['dependencies'] as $didx => $d) { if ($lidx > 0 || $didx > 0) echo ",\n"; ?>
-					'<?php echo str_replace(".", "/", $d); ?>'<?php } } echo "\n"; ?>
-				]);
-				define.amd.jQuery = true;
-			</script>
-	<?php } ?>
-	<?php
-		$scripts = $this->config->get("info.scripts", 'themes/'.$request->theme);
-		if (empty($scripts)) $scripts = array();
-	?>
-	<?php foreach ($scripts as $script) { ?>
-		<script type="text/javascript" src="<?php echo $this->url->build("app/themes/".$request->theme."/public/js/$script"); ?>"></script>
-	<?php } ?>
+	<script type="text/javascript">
+		var dojoConfig = {
+			async:true,
+			parseOnLoad:true,
+			serverTime:'<?php echo date("Y-m-d H:i:s"); ?>'
+		};
+	</script>
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<?php if (Etc::ENVIRONMENT == "production") { ?>
+
+	<script type="text/javascript" src="<?php echo $this->url->build("libraries/release/dojo/dojo/dojo.js"); ?>"></script>
+	<?php $response->js("dojo/parser"); ?>
+
+<?php } else { ?>
+
+	<script type="text/javascript" src="<?php echo $this->url->build("libraries/dojo/dojo.js"); ?>"></script>
+	<script type="text/javascript">
+		require({
+			packages: <?php echo $this->dojo->getPackages()."\n"; ?>
+		},
+			<?php echo $this->dojo->getDependencies()."\n"; ?>
+		);
+		define.amd.jQuery = true;
+	</script>
+
+<?php } ?>
+
+<?php
+	$scripts = $this->config->get("info.scripts", 'themes/'.$request->theme);
+	if (empty($scripts)) $scripts = array();
+?>
+<?php foreach ($scripts as $script) { ?>
+	<script type="text/javascript" src="<?php echo $this->url->build("app/themes/".$request->theme."/public/js/$script"); ?>"></script>
+<?php } ?>
 	<script type="text/javascript">
 		var WEBSITE_URL = '<?php echo $this->url->build(""); ?>';
 	</script>
