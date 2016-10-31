514f2e38beeb5500379f968384762e77a7805e66
Ali Gangji <ali@neonrain.com>
2016-10-31 15:18:09 -0600

    updating editable widgets

--- core/app/public/js/starbug/form/editable/Select.js
+++ core/app/public/js/starbug/form/editable/Select.js
@@ -1,10 +1,13 @@
-define(["dojo/_base/declare", "starbug/form/editable/Editable", "dijit/form/Select", "dojo/when"], function(declare, Editable, Editor, when) {
+define(["dojo/_base/declare", "starbug/form/editable/Editable", "dijit/form/Select", "dojo/when", "dstore/legacy/DstoreAdapter"], function(declare, Editable, Editor, when, DstoreAdapter) {
 	return declare([Editable], {
 		editor:Editor,
 		storeOnSave:true,
 		postCreate:function() {
 			this.inherited(arguments);
 			var self = this;
+			if (this.editorParams.collection) {
+				this.editorParams.store = new DstoreAdapter(this.editorParams.collection);
+			}
 			this.editorParams.labelAttr = 'label';
 			this.editorParams.onSetStore = function(store, items) {
 				this.options.unshift({label:'&nbsp;', value:'NULL'});
