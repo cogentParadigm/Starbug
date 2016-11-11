c92f910837c260b07f14845592b339d95d85109c
Ali Gangji <ali@neonrain.com>
2016-11-11 12:13:49 -0700

    grid based selection widget

---
+++ core/app/public/js/starbug/form/Selection.js
@@ -0,0 +1,94 @@
+define([
+	"dojo/_base/declare",
+	"dijit/_WidgetBase",
+	"dijit/_TemplatedMixin",
+	"dijit/_WidgetsInTemplateMixin",
+	"sb",
+	"dojo/text!./templates/Selection.html",
+	"put-selector/put",
+	"dojo/on",
+	"dojo/query",
+	"dojo/dom-class",
+	"dojo/_base/Deferred",
+	"dstore/Memory",
+	"starbug/form/Dialog",
+	"starbug/grid/MemoryGrid",
+	"starbug/grid/columns/handle",
+	"starbug/grid/columns/html",
+	"starbug/grid/columns/options"
+], function (declare, Widget, Templated, _WidgetsInTemplate, sb, template, put, on, query, domclass, Deferred, Memory, Dialog) {
+	return declare([Widget, Templated, _WidgetsInTemplate], {
+		value:[],
+		model:'',
+		templateString: template, //the template (./templates/FileSelect.html)
+		widgetsInTemplate: true,
+		input_name:'file',
+		size:0,
+		collection:null,
+		prefix:'',
+		postCreate:function() {
+			this.collection = new Memory({data: []});
+			this.grid.editor = self;
+			this.input.name = this.input_name;
+		},
+		startup: function() {
+			var self = this;
+			self.grid.set('collection', this.collection);
+			if (self.value.length > 0) {
+				sb.get(self.model, 'select').filter({'id':self.value.join(',')}).fetch().then(function(data) {
+					self.add(data);
+				});
+			}
+		},
+		add:function(items) {
+			var target_size = this.store.data.length + items.length;
+			if (this.size == 0) {
+				//unlimited
+			} else if (this.size == 1) {
+				this.store.setData([]);
+			} else if (target_size == this.size) {
+				this.controls.style.display = 'none';
+			} else if (target_size > this.size) {
+				alert("You have reached the limit.");
+				return;
+			} else {
+				this.controls.style.display = 'block';
+			}
+			this.set_status();
+			for (var i = 0;i<items.length;i++) {
+				this.store.put(items[i]);
+			}
+			if (items.length > 0) {
+				this.gridNode.style.display = 'block';
+			} else {
+				this.gridNode.style.display = 'none';
+			}
+			this.refresh();
+			window.dispatchEvent(new Event('resize'));
+		},
+		remove: function(item_id) {
+			this.store.remove(item_id);
+			this.refresh();
+		},
+		refresh: function() {
+			this.grid.refresh();
+			this.grid.renderArray(this.store.data);
+			this.grid.set('collection', this.collection);
+			var ids = [];
+			var items = this.store.data;
+			for (var i = 0;i<items.length;i++) {
+				ids.push(this.prefix + this.store.getIdentity(items[i]));
+			}
+			this.input.value = ids.join(',');
+		},
+		set_status: function(value) {
+			/**
+			 * updates the status indicator
+			 */
+			if (!value) this.statusNode.innerHTML = '';
+			else if (value == 'loading') {
+				this.statusNode.innerHTML = '<span class="fa fa-spinner fa-spin fa-lg"></span>';
+			}else this.statusNode.innerHTML = value;
+		}
+	});
+});
