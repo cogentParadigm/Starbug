4fe0d09d1c659e21d4c7bc1f2733691f40bb285a
Ali Gangji <ali@neonrain.com>
2016-11-18 01:34:05 -0700

    php-di configurations in db module

---
+++ modules/db/etc/di.php
@@ -0,0 +1,23 @@
+<?php
+use Interop\Container\ContainerInterface;
+return [
+	'Starbug\Db\Schema\*Interface' => DI\object('Starbug\Db\Schema\*'),
+	'Starbug\Db\Schema\SchemaInterface' => function(ContainerInterface $c) {
+		$schema = $c->get('Starbug\Db\Schema\Schema');
+		$hooks = $c->get('db.schema.hooks');
+		foreach ($hooks as $hook) {
+			$schema->addHook($hook);
+		}
+		return $schema;
+	},
+	'Starbug\Db\Schema\SchemerInterface' => function(ContainerInterface $c) {
+		$schemer = $c->get('Starbug\Db\Schema\Schemer');
+		$c->set('Starbug\Db\Schema\SchemerInterface', $schemer);
+		$migrations = $c->get('db.schema.migrations');
+		foreach ($migrations as $migration) {
+			$schemer->addMigration($migration);
+		}
+		return $schemer;
+	}
+];
+?>
