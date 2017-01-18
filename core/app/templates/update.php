8d7efa3337837be79a13403aa943bdabeecd3df3
Ali Gangji <ali@neonrain.com>
2017-01-18 10:52:34 -0700

    allow overriding form in create/update templates

--- core/app/templates/update.php
+++ core/app/templates/update.php
@@ -8,7 +8,8 @@
 	<div class="panel-heading"><strong> <span data-i18n="New <?php echo $form_header; ?>"><?php echo $form_header; ?></span></strong></div>
 	<div class="panel-body">
 <?php
-	$this->displays->render(ucwords($model)."Form", array_merge($this->request->getParameters(), array("operation" => $action, "id" => $id)));
+	if (empty($form)) $form = ucwords($model)."Form";
+	$this->displays->render($form, array_merge($this->request->getParameters(), array("operation" => $action, "id" => $id)));
 ?>
 	</div>
 </div>
