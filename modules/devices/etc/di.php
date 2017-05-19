cda226b59b5b21b9a8cb5b478ff14675048e5e23
Ali Gangji <ali@neonrain.com>
2017-05-19 14:34:07 -0600

    devices module fix

--- modules/devices/etc/di.php
+++ modules/devices/etc/di.php
@@ -3,6 +3,9 @@
 use Interop\Container\ContainerInterface;
 
 return [
+  'db.schema.migrations' => DI\add([
+    DI\get('Starbug\Comments\Migration')
+  ]),
   'Starbug\Devices\NotificationHandlerInterface' => function (ContainerInterface $c) {
     $manager = $c->get("Starbug\Devices\NotificationManager");
     $handlers = $c->get("notification.handlers");
