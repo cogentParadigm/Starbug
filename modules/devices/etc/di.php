ceb6804fc6aa587a78e0776b1c7442e5eccd24ca
Ali Gangji <ali@neonrain.com>
2017-06-09 10:30:44 -0600

    updated device notification handler configuration

--- modules/devices/etc/di.php
+++ modules/devices/etc/di.php
@@ -9,13 +9,13 @@ return [
   'Starbug\Devices\NotificationHandlerInterface' => function (ContainerInterface $c) {
     $manager = $c->get("Starbug\Devices\NotificationManager");
     $handlers = $c->get("notification.handlers");
-    foreach ($handlers as $name => $handler) {
+    foreach ($handlers as $name) {
+      $handler = $c->get("notification.handler.".$name);
       $manager->addHandler($name, $handler);
     }
     return $manager;
   },
-  'notification.handlers' => [
-    "email" => DI\get('Starbug\Devices\EmailNotificationHandler'),
-    "push" => DI\get('Starbug\Devices\PushNotificationHandler')
-  ]
+  'notification.handlers' => ["email", "push"],
+  "notification.handler.email" => DI\object('Starbug\Devices\EmailNotificationHandler'),
+  "notification.handler.push" => DI\object('Starbug\Devices\PushNotificationHandler')
 ];
