aeac22d47e261a9ef5f39b1641feb86fe2be7c3e
Ali Gangji <ali@neonrain.com>
2017-09-19 10:27:54 -0600

    select addresses collection

---
+++ modules/intl/collections/SelectAddressCollection.php
@@ -0,0 +1,18 @@
+<?php
+namespace Starbug\Intl;
+use Starbug\Core\Collection;
+use Starbug\Core\ModelFactoryInterface;
+class SelectAddressCollection extends Collection {
+  public function __construct(ModelFactoryInterface $models, AddressFormatter $address) {
+    $this->models = $models;
+    $this->address = $address;
+  }
+  public function filterRows($rows) {
+    foreach ($rows as $idx => $row) {
+      $rows[$idx] = [
+        "id" => $row["id"],
+        "label" => $this->address->format($row)
+      ];
+    }
+  }
+}
