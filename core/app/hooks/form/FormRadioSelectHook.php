aa6610e866be0afc5e5178375d03414f5674c8bb
Ali Gangji <ali@neonrain.com>
2017-10-30 11:15:39 -0600

    radio select improvements to allow passing through data to the display and collection which are producing the control

--- core/app/hooks/form/FormRadioSelectHook.php
+++ core/app/hooks/form/FormRadioSelectHook.php
@@ -17,7 +17,9 @@ class FormRadioSelectHook extends FormHook {
 			if (empty($field['query'])) $field['query'] = "select";
 		}
 		$other_option = empty($field['other_option']) ? false : $field['other_option'];
+		$display_options = empty($field['display_options']) ? [] : $field['display_options'];
 		$form->assign("value", $value);
 		$form->assign("other_option", $other_option);
+		$form->assign("display_options", $display_options);
 	}
 }
