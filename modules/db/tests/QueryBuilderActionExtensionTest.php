f4e942447cf54ee5f216caf3db4709158ea6819d
Ali Gangji <ali@neonrain.com>
2017-11-05 11:31:11 -0700

    fix QueryBuilderActionExtensionTest

--- modules/db/tests/QueryBuilderActionExtensionTest.php
+++ modules/db/tests/QueryBuilderActionExtensionTest.php
@@ -27,8 +27,8 @@ class QueryBuilderActionExtensionTest extends QueryBuilderTestBase {
                 "WHERE ".
                   "('global' LIKE `permits`.priv_type || (`permits`.priv_type='object' && `permits`.related_id=`users`.id)) AND ".
                   "(`permits`.object_deleted is null || `permits`.object_deleted=`users`.deleted) AND ".
-                  "(permits.user_groups IS NULL OR permits.user_groups IN (SELECT groups_id FROM `test_users_groups` AS `u` WHERE `u`.users_id = :default0)) AND ".
-                  "(permits.role='everyone' OR permits.role='user' && permits.who='2' OR permits.role='self' && users.id='2' OR permits.role='owner' && users.owner='2' OR (permits.role = :default1 AND (EXISTS (SELECT groups_id FROM `test_users_groups` AS `o` WHERE `o`.users_id=`users`.id AND `o`.groups_id IN (SELECT groups_id FROM `test_users_groups` AS `u` WHERE `u`.users_id = :default0)) OR NOT EXISTS (SELECT groups_id FROM `test_users_groups` AS `o` WHERE `o`.users_id=`users`.id))))";
+                  "(`permits`.user_groups IS NULL OR `permits`.user_groups IN (SELECT groups_id FROM `test_users_groups` AS `u` WHERE `u`.users_id = :default0)) AND ".
+                  "(`permits`.role='everyone' OR `permits`.role='user' && `permits`.who='2' OR `permits`.role='self' && `users`.id='2' OR `permits`.role='owner' && `users`.owner='2' OR (`permits`.role = :default1 AND (EXISTS (SELECT groups_id FROM `test_users_groups` AS `o` WHERE `o`.users_id=`users`.id AND `o`.groups_id IN (SELECT groups_id FROM `test_users_groups` AS `u` WHERE `u`.users_id = :default0)) OR NOT EXISTS (SELECT groups_id FROM `test_users_groups` AS `o` WHERE `o`.users_id=`users`.id))))";
 
     //compare
     $actual = $this->compile();
