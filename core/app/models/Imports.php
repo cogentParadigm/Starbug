a8a2495604fec655f1c2c6551f2845c444fc2903
Ali Gangji <ali@neonrain.com>
2017-12-12 09:55:59 -0700

    imports fix

--- core/app/models/Imports.php
+++ core/app/models/Imports.php
@@ -2,50 +2,50 @@
 namespace Starbug\Core;
 class Imports extends ImportsModel {
 
-	function create($import) {
-		$this->store($import);
-	}
+  function create($import) {
+    $this->store($import);
+  }
 
-	function run($import) {
-		$index = $created = $updated = 0;
-		$errors = array();
-		$import = $this->db->query("imports")->condition("id", $import['id'])->one();
-		if (empty($import['action'])) $import['action'] = "create";
-		$file = $this->db->query("files")->condition("id", $import['source'])->one();
-		$fields = $this->db->query("imports_fields")->condition("imports_id", $import['id'])->sort("position")->all();
-		$keys = $head = array();
-		foreach ($fields as $field) {
-			if ($field['update_key']) $keys[] = $field['destination'];
-		}
-		if (false !== ($handle = $this->filesystems->readStream($file["location"]."://".$file['id']."_".$file['filename'])["stream"])) {
-			$row = fgetcsv($handle);
-			foreach ($row as $idx => $column) $head[$column] = $idx;
-			while (false !== ($row = fgetcsv($handle))) {
-				$index++;
-				$record = array();
-				$updating = false;
-				foreach ($fields as $field) {
-					$record[$field['destination']] = $row[$head[$field['source']]];
-				}
-				if (!empty($keys)) {
-					$query = $this->db->query($import['model']);
-					foreach ($keys as $key) $query->condition($import['model'].".".$key, $record[$key]);
-					$exists = $query->one();
-					if ($exists) {
-						$record['id'] = $exists['id'];
-						$updating = true;
-					}
-				}
-				$this->models->get($import['model'])->{$import['action']}($record);
-				if ($this->models->get($import['model'])->errors()) {
-					$errors[$index] = $this->models->get($import['model'])->errors(false, true);
-					$this->db->errors = array();
-				} else if ($updating) {
-					$updated++;
-				} else {
-					$created++;
-				}
-			}
-		}
-	}
+  function run($import) {
+    $index = $created = $updated = 0;
+    $errors = array();
+    $import = $this->db->query("imports")->condition("id", $import['id'])->one();
+    if (empty($import['action'])) $import['action'] = "create";
+    $file = $this->db->query("files")->condition("id", $import['source'])->one();
+    $fields = $this->db->query("imports_fields")->condition("imports_id", $import['id'])->sort("position")->all();
+    $keys = $head = array();
+    foreach ($fields as $field) {
+      if ($field['update_key']) $keys[] = $field['destination'];
+    }
+    if (false !== ($handle = $this->filesystems->readStream($file["location"]."://".$file['id']."_".$file['filename']))) {
+      $row = fgetcsv($handle);
+      foreach ($row as $idx => $column) $head[$column] = $idx;
+      while (false !== ($row = fgetcsv($handle))) {
+        $index++;
+        $record = array();
+        $updating = false;
+        foreach ($fields as $field) {
+          $record[$field['destination']] = $row[$head[$field['source']]];
+        }
+        if (!empty($keys)) {
+          $query = $this->db->query($import['model']);
+          foreach ($keys as $key) $query->condition($import['model'].".".$key, $record[$key]);
+          $exists = $query->one();
+          if ($exists) {
+            $record['id'] = $exists['id'];
+            $updating = true;
+          }
+        }
+        $this->models->get($import['model'])->{$import['action']}($record);
+        if ($this->models->get($import['model'])->errors()) {
+          $errors[$index] = $this->models->get($import['model'])->errors(false, true);
+          $this->db->errors = array();
+        } else if ($updating) {
+          $updated++;
+        } else {
+          $created++;
+        }
+      }
+    }
+  }
 }
