c7c853ff87fc6c1d415c70e332aefa6bf3b5d9bb
Ali Gangji <ali@neonrain.com>
2017-12-12 09:39:02 -0700

    imports fix

--- core/app/displays/ImportsFieldsForm.php
+++ core/app/displays/ImportsFieldsForm.php
@@ -1,38 +1,40 @@
 <?php
 namespace Starbug\Core;
+
 use League\Flysystem\MountManager;
+
 class ImportsFieldsForm extends FormDisplay {
-	public $source_keys = array();
-	public $source_values = array();
-	public $model = "imports_fields";
-	public $cancel_url = "admin/imports_fields";
-	public function setFilesystems(MountManager $filesystems) {
-		$this->filesystems = $filesystems;
-	}
-	function build_display($options) {
-		$data = $this->request->getPost();
-		if ($this->success("create") && empty($data['imports_fields']['id'])) {
-			$this->request->setPost('imports_fields', 'id', $this->models->get($this->model)->insert_id);
-		}
-		$this->parse_source($options['source']);
-		$this->add(array("source", "input_type" => "select", "options" => $this->source_values));
-		$model = $this->models->get($options['model']);
-		$dest_ops = array("destination", "input_type" => "select");
-		if (method_exists($model, "import_fields")) {
-			$dest_ops = array_merge($dest_ops, $model->import_fields($options));
-		} else {
-			$dest_ops['options'] = array_keys($this->models->get($options['model'])->column_info());
-		}
-		$this->add($dest_ops);
-		$this->add(["update_key", "input_type" => "checkbox", "label" => "Use this field as a key to update records"]);
-	}
-	function parse_source($id) {
-		$file = $this->models->get("files")->load($id);
-		$head = array();
-		if (false !== ($handle = $this->filesystems->readStream($file["location"]."://".$file["id"]."_".$file["filename"])["stream"])) {
-			$head = fgetcsv($handle);
-		}
-		$this->source_keys = array_keys($head);
-		$this->source_values = array_values($head);
-	}
+  public $source_keys = array();
+  public $source_values = array();
+  public $model = "imports_fields";
+  public $cancel_url = "admin/imports_fields";
+  public function setFilesystems(MountManager $filesystems) {
+    $this->filesystems = $filesystems;
+  }
+  function build_display($options) {
+    $data = $this->request->getPost();
+    if ($this->success("create") && empty($data['imports_fields']['id'])) {
+      $this->request->setPost('imports_fields', 'id', $this->models->get($this->model)->insert_id);
+    }
+    $this->parse_source($options['source']);
+    $this->add(array("source", "input_type" => "select", "options" => $this->source_values));
+    $model = $this->models->get($options['model']);
+    $dest_ops = array("destination", "input_type" => "select");
+    if (method_exists($model, "import_fields")) {
+      $dest_ops = array_merge($dest_ops, $model->import_fields($options));
+    } else {
+      $dest_ops['options'] = array_keys($this->models->get($options['model'])->column_info());
+    }
+    $this->add($dest_ops);
+    $this->add(["update_key", "input_type" => "checkbox", "label" => "Use this field as a key to update records"]);
+  }
+  function parse_source($id) {
+    $file = $this->models->get("files")->load($id);
+    $head = array();
+    if (false !== ($handle = $this->filesystems->readStream($file["location"]."://".$file["id"]."_".$file["filename"]))) {
+      $head = fgetcsv($handle);
+    }
+    $this->source_keys = array_keys($head);
+    $this->source_values = array_values($head);
+  }
 }
