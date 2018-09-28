d6c6fd44f273db550ce71a274c3454e26b2e6a37
Ali Gangji <ali@neonrain.com>
2018-09-28 00:02:08 -0600

    update label on users grid status filter

--- modules/users/displays/UsersSearchForm.php
+++ modules/users/displays/UsersSearchForm.php
@@ -7,6 +7,6 @@ class UsersSearchForm extends SearchForm {
   public function buildDisplay($options) {
     parent::buildDisplay($options);
     $this->add(["groups", "input_type" => "text", "nolabel" => true, "data-dojo-type" => "sb/form/MultipleSelect", "data-dojo-props" => "model:'terms', query:{taxonomy:'groups'}", "data-filter" => $this->model]);
-    $this->add(["deleted", "input_type" => "select", "nolabel"  => true, "values" => ["0", "1", "0,1"], "options" => ["Active", "Deleted", "Any"], "default" => "0", "data-filter" => $this->model]);
+    $this->add(["deleted", "input_type" => "select", "nolabel"  => true, "values" => ["0", "1", "0,1"], "options" => ["Active", "Deleted", "Any Status"], "default" => "0", "data-filter" => $this->model]);
   }
 }
