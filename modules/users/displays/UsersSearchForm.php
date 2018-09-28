5d21541655597084dce023cb066761c1dc0f13a7
Ali Gangji <ali@neonrain.com>
2018-09-27 23:58:46 -0600

    users search form

--- modules/users/displays/UsersSearchForm.php
+++ modules/users/displays/UsersSearchForm.php
@@ -4,8 +4,8 @@ namespace Starbug\Users;
 use Starbug\Core\SearchForm;
 
 class UsersSearchForm extends SearchForm {
-  public function build($options = []) {
-    parent::build($options);
+  public function buildDisplay($options) {
+    parent::buildDisplay($options);
     $this->add(["groups", "input_type" => "text", "nolabel" => true, "data-dojo-type" => "sb/form/MultipleSelect", "data-dojo-props" => "model:'terms', query:{taxonomy:'groups'}", "data-filter" => $this->model]);
     $this->add(["deleted", "input_type" => "select", "nolabel"  => true, "values" => ["0", "1", "0,1"], "options" => ["Active", "Deleted", "Any"], "default" => "0", "data-filter" => $this->model]);
   }
