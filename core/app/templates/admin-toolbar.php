7fa4753be0c8c7293349d49d1bfa09c62adf29f1
Robert <robert@neonrain.com>
2018-10-04 15:59:50 -0600

    goofed on a merge resolve

--- core/app/templates/admin-toolbar.php
+++ core/app/templates/admin-toolbar.php
@@ -9,7 +9,6 @@ $new_label = "New ".$this->models->get($model)->singular_label." <b class=\"fa f
 		} else {
 			$path = $this->url->build($this->request->getPath()."/create");
 		}
-		if ($dialog) $path = "javascript:(function(){".$model."_form.show;return false;})()";
 		if ($dialog) $path = "javascript:(function(){".$model."_form.show();return false;})()";
 	?>
 	<a class="btn btn-default" href="<?php echo $path; ?>"><?php echo $new_label; ?></a>
