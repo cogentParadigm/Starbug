8d932375ef2cb2376c40a6e965b1c3707cc8d35b
Ali Gangji <ali@neonrain.com>
2018-11-11 13:50:15 -0700

    fix null handling for datetime fields

--- core/app/hooks/store/StoreDatetimeHook.php
+++ core/app/hooks/store/StoreDatetimeHook.php
@@ -3,6 +3,6 @@ namespace Starbug\Core;
 
 class StoreDatetimeHook extends QueryHook {
   public function validate($query, $key, $value, $column, $argument) {
-    return (empty($value)) ? $value : date('Y-m-d H:i:s', strtotime($value));
+    return (empty($value) || $value == "NULL") ? $value : date('Y-m-d H:i:s', strtotime($value));
   }
 }
