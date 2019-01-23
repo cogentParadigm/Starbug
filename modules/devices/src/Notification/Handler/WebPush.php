40d32e4bf982d198e311341dd30da5140e4375d9
Ali Gangji <ali@neonrain.com>
2019-01-23 11:29:34 -0700

    updated WebPush handler

--- modules/devices/src/Notification/Handler/WebPush.php
+++ modules/devices/src/Notification/Handler/WebPush.php
@@ -3,6 +3,7 @@ namespace Starbug\Devices\Notification\Handler;
 
 use Starbug\Devices\Notification\HandlerInterface;
 use Minishlink\WebPush\WebPush as WebPushClient;
+use Minishlink\WebPush\Subscription;
 use Starbug\Core\DatabaseInterface;
 
 class WebPush implements HandlerInterface {
@@ -13,14 +14,9 @@ class WebPush implements HandlerInterface {
   public function deliver($owner, $type, $subject, $body, $data = []) {
     $devices = $this->db->query("devices")->condition("owner", $owner["id"])->condition("platform", "web")->all();
     foreach ($devices as $device) {
-      $token = json_decode($device["token"]);
-      $push_data = ["endpoint" => $token->endpoint];
-      foreach ($token->keys as $k => $v) $push_data[$k] = $v;
       $result = $this->webpush->sendNotification(
-        $token->endpoint,
+        Subscription::create(json_decode($device["token"])),
         json_encode(["subject" => $subject, "body" => $body] + $data),
-        $token->keys->p256dh,
-        $token->keys->auth,
         true
       );
       if (is_array($result) && $result["expired"] === true) {
