a4653fb921b9eeeefe941e886878d17d7bd4aca5
Ali Gangji <ali@neonrain.com>
2019-01-23 11:40:48 -0700

    updated WebPush handler

--- modules/devices/src/Notification/Handler/WebPush.php
+++ modules/devices/src/Notification/Handler/WebPush.php
@@ -14,13 +14,15 @@ class WebPush implements HandlerInterface {
   public function deliver($owner, $type, $subject, $body, $data = []) {
     $devices = $this->db->query("devices")->condition("owner", $owner["id"])->condition("platform", "web")->all();
     foreach ($devices as $device) {
-      $result = $this->webpush->sendNotification(
+      $results = $this->webpush->sendNotification(
         Subscription::create(json_decode($device["token"], true)),
         json_encode(["subject" => $subject, "body" => $body] + $data),
         true
       );
-      if (is_array($result) && $result["expired"] === true) {
-        $this->db->query("devices")->condition("id", $device["id"])->delete();
+      foreach ($results as $result) {
+        if ($result->isSubscriptionExpired()) {
+          $this->db->query("devices")->condition("id", $device["id"])->delete();
+        }
       }
     }
   }
