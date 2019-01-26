87b9fbe6ec97dd374cc0c305774e477dbe3406ef
Ali Gangji <ali@neonrain.com>
2019-01-25 17:20:52 -0700

    options column update

--- core/app/public/js/starbug/grid/columns/options.js
+++ core/app/public/js/starbug/grid/columns/options.js
@@ -22,10 +22,10 @@ function(dojo, strings, put, on){
 
       //edit button
       if (column.canEdit) {
-        var href = "javascript:;";
-        if (typeof grid['dialog'] == 'string') href = 'javascript:'+grid['dialog']+'.show('+row.id+')';
-        else if (typeof grid['dialog'] == 'undefined') href = base_url+'/update/'+row.id+dojo.global.location.search;
-        var edit = put(div, 'a.Edit.btn.btn-default[title=Edit][href='+href+']', put('div.fa.fa-edit'));
+        column.editUrl = column.editUrl || "javascript:;";
+        if (typeof grid['dialog'] == 'string') column.editUrl = 'javascript:'+grid['dialog']+'.show('+row.id+')';
+        else if (typeof grid['dialog'] == 'undefined') column.editUrl = column.editUrl || base_url+'/update/${id}'+dojo.global.location.search;
+        var edit = put(div, 'a.Edit.btn.btn-default[title=Edit][href='+strings.substitute(column.editUrl, row)+']', put('div.fa.fa-edit'));
         if (typeof grid['dialog'] == "object") {
           on(edit, 'click', function(evt) {
             grid.dialog.show(row.id);
@@ -41,9 +41,9 @@ function(dojo, strings, put, on){
 
       //copy button
       if (column.canCopy) {
-        var chref = base_url + '/create?copy='+row.id;
-        if (typeof grid.dialog == 'string') chref = 'javascript:'+grid.dialog+'.show(false, {copy:'+row.id+'})';
-        var copy = put(div, 'a.Copy.btn.btn-default[title=Copy][href='+chref+']', put('div.fa.fa-files-o'));
+        column.copyUrl = column.copyUrl || base_url + '/create?copy=${id}';
+        if (typeof grid.dialog == 'string') column.copyUrl = 'javascript:'+grid.dialog+'.show(false, {copy:'+row.id+'})';
+        var copy = put(div, 'a.Copy.btn.btn-default[title=Copy][href='+string.substitute(column.copyUrl, row)+']', put('div.fa.fa-files-o'));
         if (typeof grid.dialog == "object") {
           on(copy, 'click', function(e) {
             e.preventDefault();
