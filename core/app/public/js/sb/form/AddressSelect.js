2ed5e721f82af8ce722e85f08e8e85177477aa69
Ali Gangji <ali@neonrain.com>
2019-05-15 16:53:32 -0600

    updated usages of starbug/form/Dialog to sb/modal/Form with some related revisions to facilitate the integration

--- core/app/public/js/sb/form/AddressSelect.js
+++ core/app/public/js/sb/form/AddressSelect.js
@@ -2,25 +2,30 @@ define([
   "dojo/_base/declare",
   "dojo/_base/lang",
   "./CRUDSelect",
-  "starbug/form/AddressDialog",
+  "sb/modal/AddressForm",
   "put-selector/put"
 ], function (declare, lang, CRUDSelect, Dialog, put) {
   return declare([CRUDSelect], {
     model: 'address',
     searchable: true,
     newItemLabel: 'Add New Address',
-    url: '/address/',
     dialogClass: Dialog,
+    postMixInProperties: function() {
+      this.dialogParams = this.dialogParams || {};
+      this.dialogParams.url = this.dialogParams.url || WEBSITE_URL + this.model + '/';
+      this.dialogParams.title = this.dialogParams.title || "New Address";
+      this.inherited(arguments);
+    },
     postCreate: function() {
       this.inherited(arguments);
       this.list.bodyNode.style.maxHeight = "300px";
     },
     createControlNode: function() {
-      this.controlNode = put(this.controlGroupNode, 'button.form-control.text-left[style=height:90px]');
+      this.controlNode = put(this.controlGroupNode, 'button.form-control.text-left.tl[style=height:114px]');
     },
     createToggleNode: function() {
       this.inherited(arguments);
-      put(this.toggleNode, '[style=height:90px]');
+      put(this.toggleNode, '[style=height:114px]');
     },
     renderSelection: function() {
       this.selectionNode.innerHTML = this.get('displayedValue');
