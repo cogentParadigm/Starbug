e53920a03cbb2613907f7fdfbe4ac6a0b8f389e3
Ali Gangji <ali@neonrain.com>
2019-05-16 14:42:35 -0600

    expiration date in one field on payment form

--- modules/payment/displays/PaymentForm.php
+++ modules/payment/displays/PaymentForm.php
@@ -24,8 +24,7 @@ class PaymentForm extends FormDisplay {
     $dep = ["data-dojo-type" => "starbug/form/Dependent", "data-dojo-props" => "key:'card',values:['']"];
     $this->add(["card_number", "input_type" => "text"] + $dep);
     $this->add(["card_holder", "label" => "Name on card", "input_type" => "text"] + $dep);
-    $this->add(["expiration_date[month]", "label" => "Expiration Month", "input_type" => "select", "range" => "1-12"] + $dep);
-    $this->add(["expiration_date[year]", "label" => "Expiration Year", "input_type" => "select", "range" => date("Y")."-".(intval(date("Y"))+20)] + $dep);
+    $this->add(["expiration_date", "label" => "Expiration Date", "input_type" => "text", "placeholder" => "MM/YYYY"] + $dep);
     $this->add(["cvv", "label" => "Security code", "input_type" => "text"] + $dep);
     $this->actions->add([$this->defaultAction, "class" => "btn-primary"]);
   }
