57b97cd8ff541146fa40d8343f332c4f67bf1800
Ali Gangji <ali@neonrain.com>
2019-11-26 13:44:58 -0700

    allow different paging behaviors in sb/form/_DropdownListMixin and leave store based mixins out of sb/list/Dropdown

--- core/app/public/js/sb/form/_DropdownListMixin.js
+++ core/app/public/js/sb/form/_DropdownListMixin.js
@@ -4,22 +4,29 @@ define([
   "./_SelectionMixin",
   "./_DropdownMixin",
   "../list/Dropdown",
+  "dgrid/OnDemandList",
+  "sb/grid/_StoreMixin",
   "put-selector/put",
   "dojo/on",
   "dojo/dom-class",
   "../data/Query",
   "../data/Selection",
   "sb/store/Api"
-], function (declare, lang, _SelectionMixin, _DropdownMixin, List, put, on, domclass, Query, Selection, Api) {
+], function (declare, lang, _SelectionMixin, _DropdownMixin, List, _VirtualScrolling, _StoreMixin, put, on, domclass, Query, Selection, Api) {
   return declare([_SelectionMixin, _DropdownMixin], {
     model: false,
     collection: false,
+    pagingMode: "virtual",
+    listClass: false,
     postMixInProperties: function() {
       this.inherited(arguments);
       this.query = this.query || {};
       this.query = new Query({query:this.query});
       if (false == this.collection && false !== this.model) {
-        this.collection = (new Api({model:this.model, action:"select"})).filter(this.query.query);
+        this.collectionParams = this.collectionParams || {};
+        this.collectionParams.model = this.collectionParams.model || this.model;
+        this.collectionParams.action = this.collectionParams.action || "select";
+        this.collection = (new Api(this.collectionParams)).filter(this.query.query);
       }
       this.listParams = this.listParams || {};
       this.listParams.delegate = this.selection;
@@ -27,6 +34,12 @@ define([
       this.query.on('change', lang.hitch(this, function(e) {
         this.list.set('collection', this.collection.filter(e.query));
       }));
+      this.listClass = this.listClass || List;
+      if (this.pagingMode == "virtual") {
+        this.listClass = declare([this.listClass, _VirtualScrolling]);
+      } else if (this.pagingMode == "none") {
+        this.listClass = declare([this.listClass, _StoreMixin]);
+      }
     },
     buildRendering: function() {
       this.inherited(arguments);
@@ -34,7 +47,7 @@ define([
     },
     postCreate:function() {
       this.inherited(arguments);
-      this.list = new List(this.listParams, this.listNode);
+      this.list = new this.listClass(this.listParams, this.listNode);
       this.list.startup();
     },
     open: function() {
