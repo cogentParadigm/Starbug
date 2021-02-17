86c99ceb6cf12b6ae5869873c7eb08ad93d794c9
Ali Gangji <ali@neonrain.com>
2021-02-17 01:11:31 -0700

    email templates form cleanup

--- modules/emails/displays/EmailTemplatesForm.php
+++ modules/emails/displays/EmailTemplatesForm.php
@@ -8,14 +8,14 @@ class EmailTemplatesForm extends FormDisplay {
   public $model = "email_templates";
   public $cancel_url = "admin/emails";
   public function buildDisplay($options) {
-    $this->layout->add(["top", "left" => "div.col-md-6", "right" => "div.col-md-6"]);
-    $this->layout->add(["bottom", "body" => "div.col-sm-12"]);
+    $this->layout->add(["top", "left" => "div.col-6", "right" => "div.col-6"]);
+    $this->layout->add(["bottom", "body" => "div.col-12"]);
     $this->add(["name", "pane" => "left"]);
     $this->add(["subject", "pane" => "left"]);
     $this->add(["from", "pane" => "left"]);
     $this->add(["from_name", "pane" => "left"]);
     $this->add(["cc", "input_type" => "textarea", "pane" => "right", "style" => "height:108px"]);
     $this->add(["bcc", "input_type" => "textarea", "pane" => "right", "style" => "height:108px"]);
-    $this->add(["body", "input_type" => "textarea", "pane" => "body"]);
+    $this->add(["body", "input_type" => "textarea", "class" => "rich-text", "pane" => "body"]);
   }
 }
