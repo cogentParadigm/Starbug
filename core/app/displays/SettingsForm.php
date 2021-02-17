6bd68383bb52b029e97852a2bd672fb49f07055f
Ali Gangji <ali@neonrain.com>
2021-02-17 00:32:25 -0700

    remove obsolete files

--- core/app/displays/SettingsForm.php
+++ core/app/displays/SettingsForm.php
@@ -23,10 +23,17 @@ class SettingsForm extends FormDisplay {
         $this->add([$setting["slug"], "input_type" => "html", "value" => "<h2 class=\"f7 mb2 {$marginTop} ttu\">".$setting["term"]."</h1>"]);
       }
       $field = [$setting['name'], "input_type" => $setting['type']];
-      if (!empty($setting['label'])) $field['label'] = $setting['label'];
-      if (!empty($setting['options'])) $field += json_decode($setting['options'], true);
-      if ($setting['type'] == "textarea") $field['data-dojo-type'] = 'dijit/form/Textarea';
-      elseif ($setting['type'] == "checkbox") $field['value'] = 1;
+      if (!empty($setting['label'])) {
+        $field['label'] = $setting['label'];
+      }
+      if (!empty($setting['options'])) {
+        $field += json_decode($setting['options'], true);
+      }
+      if ($setting['type'] == "textarea") {
+        $field['data-dojo-type'] = 'dijit/form/Textarea';
+      } elseif ($setting['type'] == "checkbox") {
+        $field['value'] = 1;
+      }
       $this->add($field);
     }
   }
