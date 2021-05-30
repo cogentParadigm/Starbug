be5e85c57af74187bdde45c5ba82a21b1ce0b9f6
Ali Gangji <ali@neonrain.com>
2021-05-29 18:15:20 -0600

    update price formatter for PHP 7.4 compatibility

--- modules/payment/src/PriceFormatter.php
+++ modules/payment/src/PriceFormatter.php
@@ -3,15 +3,19 @@
 // we will implement https://github.com/moneyphp/money when possible
 namespace Starbug\Payment;
 
+use NumberFormatter;
+
 class PriceFormatter implements PriceFormatterInterface {
   protected $locale;
   protected $minorUnit;
+  protected $formatter;
   public function __construct($locale, $minorUnit) {
     $this->locale = $locale;
     $this->minorUnit = $minorUnit;
+    $this->formatter = new NumberFormatter($this->locale, NumberFormatter::CURRENCY);
+    $this->formatter->setAttribute(NumberFormatter::FRACTION_DIGITS, $this->minorUnit);
   }
   public function format($price) {
-    setlocale(LC_MONETARY, $this->locale);
-    return money_format('%.2n', $price/pow(10, $this->minorUnit));
+    return $this->formatter->format($price/pow(10, $this->minorUnit));
   }
 }
