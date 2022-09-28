305a26924027f206b0b2856c72c6c91415685dfd
Ali Gangji <ali@neonrain.com>
2022-09-28 12:44:18 -0600

    NRI-546 add redis module

---
+++ modules/redis/etc/di.php
@@ -0,0 +1,39 @@
+<?php
+namespace Starbug\Redis;
+
+use DI;
+use Predis\Client;
+use Psr\Container\ContainerInterface;
+use Starbug\Queue\QueueFactory;
+
+return [
+  "redis.scheme" => "tcp",
+  "redis.host" => "127.0.0.1",
+  "redis.port" => "6379",
+  "redis.password" => "",
+  "redis.prefix" => "starbug:",
+  "Predis\Client" => function (ContainerInterface $container) {
+    return new Client(
+      [
+        "scheme" => $container->get("redis.scheme"),
+        "host" => $container->get("redis.host"),
+        "port" => $container->get("redis.port"),
+        "password" => $container->get("redis.password")
+      ],
+      [
+        "prefix" => $container->get("redis.prefix")
+      ]
+    );
+  },
+  "Starbug\Queue\*Interface" => DI\autowire("Starbug\Queue\*"),
+  "Starbug\Queue\QueueFactoryInterface" => function (ContainerInterface $container) {
+    $factory = new QueueFactory();
+    $factory->addQueue("default", function () use ($container) {
+      return $container->make("Starbug\Queue\Driver\Predis", [
+        "name" => "default",
+        "redis" => $container->get("Predis\Client")
+      ]);
+    });
+    return $factory;
+  }
+];
