29f8cb74b76e1191e860b7e145ec60b69e7362a3
Ali Gangji <ali@neonrain.com>
2022-12-10 17:40:14 -0700

    Add check for NULL to StoreDateHook

--- core/app/hooks/store/StoreDateHook.php
+++ core/app/hooks/store/StoreDateHook.php
@@ -3,6 +3,6 @@ namespace Starbug\Core;
 
 class StoreDateHook extends QueryHook {
   public function validate($query, $key, $value, $column, $argument) {
-    return (empty($value)) ? $value : date('Y-m-d', strtotime($value));
+    return (empty($value) || $value == "NULL") ? $value : date('Y-m-d', strtotime($value));
   }
 }
