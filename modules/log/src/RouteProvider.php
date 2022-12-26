8aab13d9af184c326c2a72eb4e9d554de72e8cec
Ali Gangji <ali@neonrain.com>
2022-12-26 15:05:18 -0700

    update log route provider to reflect removed removed controllers

--- modules/log/src/RouteProvider.php
+++ modules/log/src/RouteProvider.php
@@ -2,13 +2,17 @@
 namespace Starbug\Log;
 
 use Starbug\Core\Admin\RouteProvider as AdminRouteProvider;
+use Starbug\Core\Controller\ViewController;
 use Starbug\Core\Routing\Route;
 
 class RouteProvider extends AdminRouteProvider {
 
   public function configure(Route $routes) {
     $admin = $routes->getRoute("admin");
-    $admin->addRoute("/error-log", "Starbug\Core\Crud\ListController", ["model" => "error_log"]);
+    $admin->addRoute("/error-log", ViewController::class, [
+      "model" => "error_log",
+      "view" => "admin/list.html"
+    ]);
 
     $api = $routes->getRoute("api");
     $this->addAdminApiRoute($api->addRoute("/error-log/admin.{format:csv|json}"), "error_log", "AdminErrorLog");
